b3d8bce7ead8795f78b449308dddf97272cdf96d
Mike Decker <pookmish@gmail.com>
2022-11-15 11:08:47 -0800

    Extract menu button

--- components/menu/main-menu.tsx
+++ components/menu/main-menu.tsx
@@ -1,24 +1,25 @@
 import {DrupalMenuLinkContent} from "next-drupal";
+import Link from "next/link";
 import {useEffect, useState} from "react";
 import {Bars3Icon, ChevronDownIcon, ChevronUpIcon, XMarkIcon} from "@heroicons/react/20/solid";
 
 import {useAppContext} from "../../context/state";
 import getActiveTrail from "@/lib/menu";
-import Link from "next/link";
 import SearchWorks from "@/components/search/search-works";
 
 export const MainMenu = ({...props}) => {
   const appContext = useAppContext();
   const activeTrail = getActiveTrail(appContext.menu);
+
   const [menuOpen, setMenuOpen] = useState(false)
   const [menuTree, setMenuTree] = useState(appContext.menu)
-
-  useEffect(() => {
-    if (menuTree.length === 0) {
-      fetch('/api/menu').then(response => response.json())
-        .then(menuTree => setMenuTree(menuTree))
-    }
-  }, [menuTree]);
+  //
+  // useEffect(() => {
+  //   if (menuTree.length === 0) {
+  //     fetch('/api/menu').then(response => response.json())
+  //       .then(menuTree => setMenuTree(menuTree))
+  //   }
+  // }, [menuTree]);
 
   if (menuTree.length === 0) {
     return null;
@@ -26,7 +27,7 @@ export const MainMenu = ({...props}) => {
 
   return (
     <div {...props}>
-      <Link href="#" className="lg:su-hidden su-text-black-true su-absolute su-top-0 su-right-20 su-no-underline"
+      <button className="lg:su-hidden su-text-black-true su-absolute su-top-0 su-right-20 su-no-underline"
             onClick={() => setMenuOpen(!menuOpen)}>
         {!menuOpen &&
             <>
@@ -40,11 +41,11 @@ export const MainMenu = ({...props}) => {
               <span className="su-text-14">Close<span className="su-sr-only"> Menu</span></span>
             </>
         }
-      </Link>
+      </button>
 
       <div className="su-relative">
         <div
-          className={"su-py-20 su-border-t-4 lg:su-border-0 su-border-cardinal-red su-bg-black-true lg:su-bg-transparent su-absolute lg:su-relative su-top-0 su-w-full su-z-10 lg:su-block " + (menuOpen ? "su-block" : "su-hidden")}>
+          className={"su-py-20 lg:su-pb-0 su-border-t-4 lg:su-border-0 su-border-cardinal-red su-bg-black-true lg:su-bg-transparent su-absolute lg:su-relative su-top-0 su-w-full su-z-10 lg:su-block " + (menuOpen ? "su-block" : "su-hidden")}>
           <SearchWorks className="lg:su-hidden"/>
 
           <ul className="su-m-0 su-p-0 su-list-unstyled lg:su-flex lg:su-justify-end">
@@ -93,42 +94,48 @@ const MenuItem = ({title, url, items, active, menuLevel = 0}: MenuItemProps) => 
     'su-ml-[80px]',
     'su-ml-[120px]'
   ];
+  const belowItems = items?.length > 0 ? items : false;
 
   return (
     <li className={"su-m-0 su-p-0 su-relative lg:su-flex lg:su-flex-wrap"}>
       <Link href={url}
-            className={"su-block su-text-white lg:su-text-black-true hover:su-text-white hover:su-bg-black lg:hover:su-text-black-true lg:hover:su-bg-transparent su-no-underline lg:hover:su-underline su-w-full su-p-20 su-border-b su-border-black su-w-[calc(100%-70px)]" + (menuLevel >= 1 ? " lg:su-border-fog-light" : " lg:su-border-0") + (items?.length > 0 ? " lg:su-pr-5" : "")}>
+            className={"su-block su-text-white lg:su-text-black-true hover:su-text-white hover:su-bg-black lg:hover:su-text-black-true lg:hover:su-bg-transparent su-no-underline lg:hover:su-underline su-w-full su-p-20 su-border-b su-border-black lg:su-w-[calc(100%-70px)]" + (menuLevel >= 1 ? " lg:su-border-fog-light" : " lg:su-border-0")}>
         <span className={"su-block su-pl-30 lg:su-pl-0 su-ml-[" + (menuLevel * 30) + "px] lg:su-ml-0"}>{title}</span>
       </Link>
 
-      {(items?.length > 0) &&
-          <>
-            <button
-                className={"su-bg-black su-h-[70px] lg:su-h-auto su-w-[70px] su-absolute lg:su-relative su-z-20 su-top-0 su-right-0 " + (menuLevel >= 1 ? 'lg:su-bg-fog-light' : 'lg:su-bg-transparent')}
-                onClick={() => setSubmenuOpen(!submenuOpen)}>
-              {submenuOpen &&
-                  <>
-                    <ChevronUpIcon className="su-text-white lg:su-text-black-true su-mx-auto" height={40}/>
-                    <span className="su-sr-only">Collapse submenu</span>
-                  </>
-              }
-
-              {!submenuOpen &&
-                  <>
-                    <ChevronDownIcon className="su-text-white lg:su-text-black-true su-mx-auto" height={40}/>
-                    <span className="su-sr-only">Expand submenu</span>
-                  </>
-              }
-            </button>
-
-            <ul className={"su-w-full su-m-0 su-p-0 su-list-unstyled lg:su-bg-white lg:su-top-full lg:su-w-[150%]" + (submenuOpen ? " su-block" : " su-hidden") + (menuLevel < 1 ? " lg:su-absolute" : "")}
-                role="menu">
-              {items.map((item, i) =>
-                <MenuItem key={item.id} {...item} menuLevel={menuLevel + 1}/>)
-              }
-            </ul>
-          </>
+      {belowItems &&
+        <>
+          <MenuButton isOpen={submenuOpen} menuLevel={menuLevel} onButtonClick={() => setSubmenuOpen(!submenuOpen)}/>
+          <ul className={"su-w-full su-m-0 su-p-0 su-list-unstyled lg:su-bg-white lg:su-top-full lg:su-w-[150%]" + (submenuOpen ? " su-block" : " su-hidden") + (menuLevel < 1 ? " lg:su-absolute" : "")}
+              role="menu">
+            {belowItems.map((item, i) =>
+              <MenuItem key={item.id} {...item} menuLevel={menuLevel + 1}/>)
+            }
+          </ul>
+        </>
       }
     </li>
+  )
+}
+
+const MenuButton = ({isOpen, onButtonClick, menuLevel}) => {
+  return (
+    <button
+      className={"su-bg-black su-h-[70px] lg:su-h-auto su-w-[70px] su-absolute lg:su-relative su-z-20 su-top-0 su-right-0 " + (menuLevel >= 1 ? 'lg:su-bg-fog-light' : 'lg:su-bg-transparent')}
+      onClick={onButtonClick}>
+      {isOpen &&
+          <>
+            <ChevronUpIcon className="su-text-white lg:su-text-black-true su-mx-auto" height={40}/>
+            <span className="su-sr-only">Collapse submenu</span>
+          </>
+      }
+
+      {!isOpen &&
+          <>
+            <ChevronDownIcon className="su-text-white lg:su-text-black-true su-mx-auto" height={40}/>
+            <span className="su-sr-only">Expand submenu</span>
+          </>
+      }
+    </button>
   )
 }
--- components/search/search-works.tsx
+++ components/search/search-works.tsx
@@ -1,13 +1,11 @@
 import {MagnifyingGlassIcon} from "@heroicons/react/20/solid";
 
 const SearchWorks = (props) => {
-  const inputId = 'searchworks-' + Math.random().toString(16).slice(2);
-
   return (
     <div {...props}>
       <form action="https://searchworks.stanford.edu" className="su-relative su-mb-20 su-mx-40">
-        <label htmlFor={inputId} className="su-sr-only">Text search</label>
-        <input id={inputId} name="q" className="su-input su-w-full su-p-10 su-rounded-full"
+        <label htmlFor="searchworks-search" className="su-sr-only">Text search</label>
+        <input id="searchworks-search" name="q" className="su-input su-w-full su-p-10 su-rounded-full"
                placeholder="Search this site"/>
         <button className="su-bg-cardinal-red su-rounded-full su-p-5 su-absolute su-top-5 su-right-10">
           <MagnifyingGlassIcon height={20} className="su-text-white"/>
