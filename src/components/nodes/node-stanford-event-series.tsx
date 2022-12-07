4ac846092b5a66b2977bd7602d6954bbe8bbc0ac
jdwjdwjdw <jacobdeanwilson@gmail.com>
2022-12-07 18:09:46 +0000

    Update event series node

--- src/components/nodes/node-stanford-event-series.tsx
+++ src/components/nodes/node-stanford-event-series.tsx
@@ -1,6 +1,5 @@
 import {EventSeries} from "../../types/drupal";
 import {Paragraph} from "@/components/paragraphs";
-import {DrupalLink} from "@/components/simple/link";
 import Link from "next/link";
 import {MainContentLayout} from "@/components/layouts/main-content-layout";
 import {NodeListDisplay} from "@/nodes/index";
@@ -15,37 +14,44 @@ export const NodeStanfordEventSeries = ({node, ...props}: EventSeriesNodeProps) 
   return (
     <MainContentLayout pageTitle={node.title}>
       <article {...props}>
-        <div>
-          {node.su_event_series_subheadline}
-        </div>
-        <div>
-          {node.su_event_series_dek}
-        </div>
-        <div>
-          {node.su_event_series_components && node.su_event_series_components.map(paragraph =>
-            <Paragraph key={paragraph.id} paragraph={paragraph}/>
-          )}
-        </div>
-        {node.su_event_series_event && 
-          <div className={"su-my-40 su-grid su-gap-xl"}>
-            {node.su_event_series_event.map(item =>
-            <div className={"su-pb-50 su-mb-50 last:su-pb-0 su-border-[#c6c6c6] last:su-border-none su-border-b"}>
-              <NodeListDisplay node={item} key={item.id}/>
-            </div>
+        <Conditional showWhen={node.su_event_series_subheadline}>
+          <h2 className="su-type-3 su-rs-mb-1">
+            {node.su_event_series_subheadline}
+          </h2>
+        </Conditional>
+        <Conditional showWhen={node.su_event_series_dek}>
+          <div className="su-rs-mb-4 su-text-16 md:su-text-21">
+            {node.su_event_series_dek}
+          </div>
+        </Conditional>
+        <Conditional showWhen={node.su_event_series_components}>
+          <div>
+            {node.su_event_series_components.map(paragraph =>
+              <Paragraph key={paragraph.id} paragraph={paragraph}/>
             )}
           </div>
-        }
+        </Conditional>
+        <Conditional showWhen={node.su_event_series_event}>
+          <div className={"md:su-cc su-rs-my-6 su-grid su-gap-xl"}>
+            {node.su_event_series_event.map(item =>
+              <div className={"su-pb-50 su-mb-50 last:su-pb-0 su-border-[#c6c6c6] last:su-border-none su-border-b"}>
+                <NodeListDisplay node={item} key={item.id}/>
+              </div>
+            )}
+          </div>
+        </Conditional>
       </article>
     </MainContentLayout>
   )
 }
 
 export const NodeStanfordEventSeriesListItem = ({node, ...props}: EventSeriesNodeProps) => {
+  // Not being utilized anywhere currently
   return (
     <article {...props}>
-      <DrupalLink href={node.path.alias}>
+      <Link href={node.path.alias}>
         <h2 className="su-text-cardinal-red">{node.title}</h2>
-      </DrupalLink>
+      </Link>
     </article>
   )
 }
