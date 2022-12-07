a1c85a3b014b55ce63866915f86267c79da8f9bc
jdwjdwjdw <jacobdeanwilson@gmail.com>
2022-12-07 20:15:51 +0000

    Update course displays and conditionals

--- src/components/nodes/node-stanford-course.tsx
+++ src/components/nodes/node-stanford-course.tsx
@@ -1,7 +1,9 @@
 import {DrupalLink} from "@/components/simple/link";
+import Link from "next/link";
 import formatHtml from "@/lib/format-html";
 import {Course} from "../../types/drupal";
 import {MainContentLayout} from "@/components/layouts/main-content-layout";
+import Conditional from "@/components/simple/conditional";
 
 interface CourseNodeProps {
   node: Course;
@@ -13,21 +15,43 @@ export const NodeStanfordCourse = ({node, ...props}: CourseNodeProps) => {
     <MainContentLayout>
       <article>
         <h1>{node.title}</h1>
-        {node.su_course_academic_year}
-        {node.body && <>{formatHtml(node.body.processed)}</>}
-        {node.su_course_code}
-        {node.su_course_id}
+        <Conditional showWhen={node.su_course_academic_year}>
+          {node.su_course_academic_year}
+        </Conditional>
+        <Conditional showWhen={node.body}>
+          <>{formatHtml(node.body.processed)}</>
+        </Conditional>
+        <Conditional showWhen={node.su_course_code}>
+          {node.su_course_code}
+        </Conditional>
+        <Conditional showWhen={node.su_course_id}>
+          {node.su_course_id}
+        </Conditional>
         {node.su_course_link && <DrupalLink href={node.su_course_link.url}>{node.su_course_link.title}</DrupalLink>}
-        {node.su_course_quarters && node.su_course_quarters.map(term =>
-          <div key={term.id}>{term.name}</div>
-        )}
-        {node.su_course_subject && <div>{node.su_course_subject.name}</div>}
-        {node.su_course_tags && node.su_course_tags.map(term => <div key={term.id}>{term.name}</div>)}
-        {node.su_course_instructors && node.su_course_instructors.map((instructor, index) => <div
-          key={`course-instructor-${index}`}>{instructor}</div>)}
-        {node.su_shared_tags && node.su_shared_tags.map(term => <div key={term.id}>{term.name}</div>)}
-        {node.su_course_section_units}
-
+        {/* <Conditional showWhen={node.su_course_link}>
+          <DrupalLink href={node.su_course_link.url}>{node.su_course_link.title}</DrupalLink>
+        </Conditional> */}
+        <Conditional showWhen={node.su_course_quarters}>
+          {node.su_course_quarters.map(term =>
+            <div key={term.id}>{term.name}</div>
+          )}
+        </Conditional>
+        <Conditional showWhen={node.su_course_subject}>
+          <div>{node.su_course_subject.name}</div>
+        </Conditional>
+        <Conditional showWhen={node.su_course_tags}>
+          {node.su_course_tags.map(term => <div key={term.id}>{term.name}</div>)}
+        </Conditional>
+        <Conditional showWhen={node.su_course_instructors}>
+          {node.su_course_instructors.map((instructor, index) => <div
+            key={`course-instructor-${index}`}>{instructor}</div>)}
+        </Conditional>
+        <Conditional showWhen={node.su_shared_tags}>
+          {node.su_shared_tags.map(term => <div key={term.id}>{term.name}</div>)}
+        </Conditional>
+        <Conditional showWhen={node.su_course_section_units}>
+          {node.su_course_section_units}
+        </Conditional>
       </article>
     </MainContentLayout>
   )
@@ -36,9 +60,9 @@ export const NodeStanfordCourse = ({node, ...props}: CourseNodeProps) => {
 export const NodeStanfordCourseListItem = ({node, ...props}: CourseNodeProps) => {
   return (
     <article {...props}>
-      <DrupalLink href={node.path.alias}>
+      <Link href={node.path.alias}>
         <h2 className="su-text-cardinal-red">{node.title}</h2>
-      </DrupalLink>
+      </Link>
     </article>
   )
 }
@@ -46,10 +70,10 @@ export const NodeStanfordCourseListItem = ({node, ...props}: CourseNodeProps) =>
 export const NodeStanfordCourseCard = ({node, ...props}: CourseNodeProps) => {
   return (
     <article className="su-shadow-lg" {...props}>
-      <DrupalLink href={node.path.alias}
+      <Link href={node.path.alias}
                   className="su-no-underline su-text-cardinal-red hover:su-underline hover:su-text-black">
         <h2 className="su-text-cardinal-red">{node.title}</h2>
-      </DrupalLink>
+      </Link>
     </article>
   )
 }
