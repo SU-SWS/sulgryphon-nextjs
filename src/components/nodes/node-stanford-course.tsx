21d523f5dcefd9b0f748082822786cb9054a93bf
jdwjdwjdw <jacobdeanwilson@gmail.com>
2022-12-08 22:51:09 +0000

    Add course list config and styling

--- src/components/nodes/node-stanford-course.tsx
+++ src/components/nodes/node-stanford-course.tsx
@@ -12,9 +12,9 @@ interface CourseNodeProps {
 export const NodeStanfordCourse = ({node, ...props}: CourseNodeProps) => {
 
   return (
-    <MainContentLayout>
-      <article>
-        <h1>{node.title}</h1>
+    <MainContentLayout pageTitle={node.title}>
+      <article {...props}>
+        { console.log(node) }
         <Conditional showWhen={node.su_course_academic_year}>
           {node.su_course_academic_year}
         </Conditional>
@@ -60,9 +60,35 @@ export const NodeStanfordCourse = ({node, ...props}: CourseNodeProps) => {
 export const NodeStanfordCourseListItem = ({node, ...props}: CourseNodeProps) => {
   return (
     <article {...props}>
-      <Link href={node.path.alias}>
-        <h2 className="su-text-cardinal-red">{node.title}</h2>
+      { console.log(node) }
+      <span className="su-font-bold su-leading-cozy">
+        <Conditional showWhen={node.su_course_subject.name}>
+          {node.su_course_subject.name}{' '}
+        </Conditional>
+        <Conditional showWhen={node.su_course_code}>
+          {node.su_course_code}
+        </Conditional>
+        <Conditional showWhen={node.su_course_academic_year}>
+          <span className="su-font-normal" >
+            {' | '}{node.su_course_academic_year}
+          </span>
+        </Conditional>
+      </span>
+      <Link href={node.path.alias} className="su-text-digital-red hocus:su-text-digital-red su-no-underline hocus:su-underline">
+        <h2 className="su-type-2">{node.title}</h2>
       </Link>
+      <Conditional showWhen={node.su_course_instructors}>
+        <div className="su-mb-20 sm:su-flex">
+          <h3 className="su-mb-0 su-font-bold su-leading-snug su-mr-[10px] su-text-16 xl:su-text-18 2xl:su-text-19">Instructors: </h3>
+          {node.su_course_instructors.map((instructor, index) =>
+            <span key={`course-instructor-${index}`} className="su-leading-cozy su-text-16 xl:su-text-18 2xl:su-text-19 su-font-normal">{(index ? ', ' : '') + instructor}</span>
+          )}
+        </div>
+      </Conditional>
+      { node.body && <>{formatHtml(node.body.processed)}</>}
+      {/* <Conditional showWhen={node.body}>
+        <>{formatHtml(node.body.processed)}</>
+      </Conditional> */}
     </article>
   )
 }
