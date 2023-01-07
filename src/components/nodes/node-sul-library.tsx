2fce868cff104dbb957d972373fed2980f3572d8
Jacob Wilson <jacobdeanwilson@gmail.com>
2023-01-07 01:05:55 +0000

    Update library branch node page

--- src/components/nodes/node-sul-library.tsx
+++ src/components/nodes/node-sul-library.tsx
@@ -3,6 +3,7 @@ import {Library} from "../../types/drupal";
 import formatHtml from "@/lib/format-html";
 import {DrupalLink} from "@/components/simple/link";
 import {MainContentLayout} from "@/components/layouts/main-content-layout";
+import {Paragraph} from "@/components/paragraphs";
 import axios from "axios";
 
 
@@ -28,19 +29,30 @@ export const NodeSulLibrary = ({node, ...props}: SulLibraryNodeProps) => {
   return (
     <MainContentLayout pageTitle={node.title}>
       <article>
-        {node.body && <div>{formatHtml(node.body.processed)}</div>}
-
-        {node.su_library__access && <div><h2>About</h2>{node.su_library__access}</div>}
-        {node.su_library__accessibility && <div><h2>Accessibility</h2>{node.su_library__accessibility}</div>}
-        {node.su_library__email && <div><h2>Email</h2>{node.su_library__email}</div>}
-        {node.su_library__location && <div><h2>Location</h2>{node.su_library__location}</div>}
-        {node.su_library__parking && <div><h2>Transit and Parking</h2>{node.su_library__parking}</div>}
+        { console.log(node) }
+        {/* {node.body && <div>{formatHtml(node.body.processed)}</div>} */}
+        {/* {node.su_library__access && <div><h2>About</h2>{node.su_library__access}</div>} */}
+        {/* {node.su_library__accessibility && <div><h2>Accessibility</h2>{node.su_library__accessibility}</div>} */}
         {node.su_library__phone && <div><h2>Phone</h2>{node.su_library__phone}</div>}
+        {node.su_library__email && <div><h2>Email</h2>{node.su_library__email}</div>}
+        {node.su_library__address &&
+          <>
+            <h2>Location</h2>
+              <div className="su-text-16 md:su-text-18">{node.su_library__address.address_line1}</div>
+              <div className="su-text-16 md:su-text-18">{node.su_library__address.address_line2}</div>
+              <div className="su-text-16 md:su-text-18">{node.su_library__address.locality}, {node.su_library__address.administrative_area} {node.su_library__address.postal_code}</div>
+          </>
+        }
+        {node.su_library__parking && <div><h2>Transit and Parking</h2>{node.su_library__parking}</div>}
 
         <div>
           <h2>Hours</h2>
           {hours && <LibraryHours {...todayHours} />}
         </div>
+
+        {node.su_library__paragraphs && node.su_library__paragraphs.map(paragraph =>
+          <Paragraph key={paragraph.id} paragraph={paragraph}/>
+        )}
 
       </article>
     </MainContentLayout>
