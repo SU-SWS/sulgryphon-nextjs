ade492df0414851899374d65177b7e8ae031bf56
jdwjdwjdw <jacobdeanwilson@gmail.com>
2023-01-10 22:39:31 +0000

    Update library branch page

--- src/components/nodes/node-sul-library.tsx
+++ src/components/nodes/node-sul-library.tsx
@@ -1,18 +1,24 @@
 import {useEffect, useState} from "react";
 import {Library} from "../../types/drupal";
 import formatHtml from "@/lib/format-html";
+import {ReactNodeLike} from "prop-types";
+import Image from "next/image";
+import Link from "next/link";
 import {DrupalLink} from "@/components/simple/link";
 import {MainContentLayout} from "@/components/layouts/main-content-layout";
+import Conditional from "@/components/simple/conditional";
 import {Paragraph} from "@/components/paragraphs";
+import {Banner} from "@/components/patterns/banner";
 import {ClockIcon, EnvelopeIcon, MapPinIcon, PhoneIcon } from "@heroicons/react/24/outline";
 import axios from "axios";
 
 
 interface SulLibraryNodeProps {
   node: Library;
+  image?: ReactNodeLike;
 }
 
-export const NodeSulLibrary = ({node, ...props}: SulLibraryNodeProps) => {
+export const NodeSulLibrary = ({node, image, ...props}: SulLibraryNodeProps) => {
   const [hours, setHours] = useState(null)
 
   useEffect(() => {
@@ -36,6 +42,69 @@ export const NodeSulLibrary = ({node, ...props}: SulLibraryNodeProps) => {
         {/* {node.su_library__accessibility && <div><h2>Accessibility</h2>{node.su_library__accessibility}</div>} */}
         {/* {node.su_library__parking && <div><h2>Transit and Parking</h2>{node.su_library__parking}</div>} */}
 
+        {/* <Banner
+          // image={image}
+          header={node.su_library__phone}
+          superHeader={node.su_library__phone}
+          body={node.su_library__phone}
+          // link={node.su_library__phone}
+          // overlayPosition={paragraph.behavior_settings?.hero_pattern?.overlay_position}
+          {...props}
+        /> */}
+
+        <div className={`hero su-basefont-23 su-relative su-h-full su-mx-auto su-w-full lg:su-max-h-500}`}>
+          <div className="su-w-full su-overflow-hidden su-relative su-max-h-500 su-min-h-[30rem] lg:su-min-h-[50rem] su-px-[50px] lg:su-p-0 su-bg-[grey]">
+            {/* {image} */}
+            image
+          </div>
+
+          <div className='su-cc su-mx-auto su-block lg:su-absolute lg:su-top-auto lg:su-bottom-36 lg:su-right-36'>
+            <div className="card su-block su-w-full su-basefont-23 su-leading-display su-bg-white su-text-black su-border su-border-solid su-border-black-10 su-shadow-md">
+              <div className="su-overflow-hidden su-aspect-[16/9] su-relative">
+                image
+              </div>
+              <div className="card-body su-items-start su-rs-px-2 su-rs-pt-2 su-rs-pb-4">
+                {node.su_library__phone && 
+                  <div className="su-relative su-flex su-flex-row su-items-start su-mt-40 md:su-mt-20 su-mb-4">
+                    <PhoneIcon width={26} className="md:su-absolute md:su-left-[-32px] su-mr-3 md:su-mr-0"/>
+                    {node.su_library__phone}
+                  </div>
+                }
+                {node.su_library__email && 
+                  <div className="su-relative su-flex su-flex-row su-items-start su-mt-40 md:su-mt-20 su-mb-4">
+                    <EnvelopeIcon width={26} className="md:su-absolute md:su-left-[-32px] su-mr-3 md:su-mr-0"/>
+                    {node.su_library__email}
+                  </div>
+                }
+                {node.su_library__address &&
+                  <div className="su-relative su-mt-40 md:su-mt-20 su-mb-4">
+                    <MapPinIcon width={26} className="md:su-absolute md:su-left-[-32px] su-mr-3 md:su-mr-0"/>
+                    {node.su_library__map_link ? (
+                      <Link href={node.su_library__map_link.uri} className="su-no-underline hocus:su-underline">
+                        <div>{node.su_library__address.address_line1}</div>
+                        <div>{node.su_library__address.address_line2}</div>
+                        <div>{node.su_library__address.locality}, {node.su_library__address.administrative_area} {node.su_library__address.postal_code}</div>
+                      </Link>
+                    ): (
+                      <>
+                        <div>{node.su_library__address.address_line1}</div>
+                        <div>{node.su_library__address.address_line2}</div>
+                        <div>{node.su_library__address.locality}, {node.su_library__address.administrative_area} {node.su_library__address.postal_code}</div>
+                      </>
+                    )}
+                  </div>
+                }
+                {hours &&
+                  <div className="su-relative su-mt-40 md:su-mt-20 su-mb-4">
+                    <ClockIcon width={26} className="md:su-absolute md:su-left-[-32px] su-mr-3 md:su-mr-0"/>
+                    {hours && <LibraryHours {...todayHours} />}
+                  </div>
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div className="su-block su-w-full su-basefont-23 su-leading-display su-bg-white su-text-black su-border su-border-solid su-border-black-10 su-shadow-md su-rs-pt-2 su-rs-px-2 su-rs-pb-3">
           {node.su_library__phone && 
             <div className="su-relative su-flex su-flex-row su-items-start su-mt-40 md:su-mt-20 su-mb-4">
@@ -52,9 +121,19 @@ export const NodeSulLibrary = ({node, ...props}: SulLibraryNodeProps) => {
           {node.su_library__address &&
             <div className="su-relative su-mt-40 md:su-mt-20 su-mb-4">
               <MapPinIcon width={26} className="md:su-absolute md:su-left-[-32px] su-mr-3 md:su-mr-0"/>
-              <div className="su-text-16 md:su-text-18">{node.su_library__address.address_line1}</div>
-              <div className="su-text-16 md:su-text-18">{node.su_library__address.address_line2}</div>
-              <div className="su-text-16 md:su-text-18">{node.su_library__address.locality}, {node.su_library__address.administrative_area} {node.su_library__address.postal_code}</div>
+              {node.su_library__map_link ? (
+                <Link href={node.su_library__map_link.uri} className="su-no-underline hocus:su-underline">
+                  <div>{node.su_library__address.address_line1}</div>
+                  <div>{node.su_library__address.address_line2}</div>
+                  <div>{node.su_library__address.locality}, {node.su_library__address.administrative_area} {node.su_library__address.postal_code}</div>
+                </Link>
+              ): (
+                <>
+                  <div>{node.su_library__address.address_line1}</div>
+                  <div>{node.su_library__address.address_line2}</div>
+                  <div>{node.su_library__address.locality}, {node.su_library__address.administrative_area} {node.su_library__address.postal_code}</div>
+                </>
+              )}
             </div>
           }
           {hours &&
