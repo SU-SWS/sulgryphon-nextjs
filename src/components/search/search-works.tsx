822b16ce0b6b76b70dcbfc7e52785caffe9c4b26
Mike Decker <pookmish@gmail.com>
2023-01-18 09:34:09 -0800

    unique IDs on search input fields

--- src/components/search/search-works.tsx
+++ src/components/search/search-works.tsx
@@ -1,11 +1,13 @@
 import {MagnifyingGlassIcon} from "@heroicons/react/20/solid";
+import {useId} from "react";
 
-const SearchWorks = ({placeholder="Search", ...props}) => {
+const SearchWorks = ({placeholder = "Search", ...props}) => {
+  const inputId = useId();
   return (
     <div {...props}>
       <form action="https://searchworks.stanford.edu" className="su-relative">
-        <label htmlFor="searchworks-search" className="su-sr-only">Text search</label>
-        <input id="searchworks-search" name="q" className="su-input su-w-full su-p-10 su-rounded"
+        <label htmlFor={inputId + '-searchworks'} className="su-sr-only">Text search</label>
+        <input id={inputId + '-searchworks'} name="q" className="su-input su-w-full su-p-10 su-rounded"
                placeholder={placeholder}/>
         <button className="su-bg-cardinal-red su-rounded-full su-p-5 su-absolute su-top-5 su-right-10">
           <MagnifyingGlassIcon height={20} className="su-text-white"/>
