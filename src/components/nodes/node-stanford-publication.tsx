c220787c2669911a49fe9df293d45cfbaff25dd5
jdwjdwjdw <jacobdeanwilson@gmail.com>
2023-02-08 20:21:26 +0000

    Update node page, add related topics

--- src/components/nodes/node-stanford-publication.tsx
+++ src/components/nodes/node-stanford-publication.tsx
@@ -21,74 +21,74 @@ export const NodeStanfordPublication = ({node, ...props}: PublicationNodeProps) 
   return (
     <MainContentLayout pageTitle={node.title}>
       {console.log('node', node)}
-      <article {...props}>
-      {node.su_publication_topics && node.su_publication_topics.map(term=>
-        <div key={term.id} className="su-text-16 md:su-text-18 2xl:su-text-19 su-rs-mb-2">{term.name}</div>
-      )}
-      <div className="md:su-grid su-grid-cols-6 su-gap-[40px]">
-          <div className="su-col-span-4">
-            <div className="su-rs-mb-7 md:su-w-11/12 ">
-
-              {node.su_publication_components && <Rows rows={node.su_publication_components} rowField="su_pubs_components"/>}
-            </div>
+        <article {...props}>
+        {node.su_publication_citation.citation_type.resourceIdObjMeta.drupal_internal__target_id && 
+          <div className="su-text-16 md:su-text-18 2xl:su-text-19 su-rs-mb-2">
+            {node.su_publication_citation.citation_type.resourceIdObjMeta.drupal_internal__target_id}
           </div>
+        }
+        <div className="md:su-grid su-grid-cols-6 su-gap-[40px] su-rs-mb-7">
+            <div className="su-col-span-4">
+              <div className="su-rs-mb-7 md:su-w-11/12 ">
 
-          <div className="su-col-span-2">
-            <div className="su-rs-pl-3 su-border-l su-border-black-10">
-              <Conditional showWhen={node.su_publication_citation.su_author.length > 0}>
-                <div className="su-rs-mb-2">
-                    <h2 className="su-type-0">Author(s)</h2>
-                    <div>
-                      {node.su_publication_citation.su_author && node.su_publication_citation.su_author.map((author, index) =>
-                        <div key={`citation-author-${index}`}>
-                          {author.given} {author.family}
-                        </div>
-                      )}
-                    </div>
-                </div>
-              </Conditional>
-
-              <Conditional showWhen={node.su_publication_citation.su_publisher}>
-                <div className="su-rs-mb-2">
-                  <h2 className="su-type-0">Publisher</h2>
-                  {node.su_publication_citation.su_publisher}
-                </div>
-              </Conditional>
-
-              <Conditional showWhen={node.su_publication_citation.su_year}>
-                <div className="su-rs-mb-4">
-                  <h2 className="su-type-0">Publication Date</h2>
-                  <Conditional showWhen={node.su_publication_citation.su_month && node.su_publication_citation.su_day}>
-                    {getMonthName(node.su_publication_citation.su_month)} {node.su_publication_citation.su_day}, {node.su_publication_citation.su_year}
-                  </Conditional>
-                  <Conditional showWhen={node.su_publication_citation.su_month && !node.su_publication_citation.su_day}>
-                    {node.su_publication_citation.su_month}, {node.su_publication_citation.su_year}
-                  </Conditional>
-                  <Conditional showWhen={!node.su_publication_citation.su_month && !node.su_publication_citation.su_day}>
-                    {node.su_publication_citation.su_year}
-                  </Conditional>
-                </div>
-              </Conditional>
+                {node.su_publication_components && <Rows rows={node.su_publication_components} rowField="su_pubs_components"/>}
+              </div>
             </div>
 
-            <Conditional showWhen={node.su_publication_cta}>
-              <div className="su-rs-pl-3">
-                <DrupalLinkButton href={node.su_publication_cta.url}>{node.su_publication_cta.title}</DrupalLinkButton>
-              </div>
-            </Conditional>
+            <div className="su-col-span-2">
+              <div className="su-rs-pl-3 su-border-l su-border-black-10">
+                <Conditional showWhen={node.su_publication_citation.su_author.length > 0}>
+                  <div className="su-rs-mb-2">
+                      <h2 className="su-type-0">Author(s)</h2>
+                      <div>
+                        {node.su_publication_citation.su_author && node.su_publication_citation.su_author.map((author, index) =>
+                          <div key={`citation-author-${index}`}>
+                            {author.given} {author.family}
+                          </div>
+                        )}
+                      </div>
+                  </div>
+                </Conditional>
 
+                <Conditional showWhen={node.su_publication_citation.su_publisher}>
+                  <div className="su-rs-mb-2">
+                    <h2 className="su-type-0">Publisher</h2>
+                    {node.su_publication_citation.su_publisher}
+                  </div>
+                </Conditional>
+
+                <Conditional showWhen={node.su_publication_citation.su_year}>
+                  <div className="su-rs-mb-4">
+                    <h2 className="su-type-0">Publication Date</h2>
+                    <Conditional showWhen={node.su_publication_citation.su_month && node.su_publication_citation.su_day}>
+                      {getMonthName(node.su_publication_citation.su_month)} {node.su_publication_citation.su_day}, {node.su_publication_citation.su_year}
+                    </Conditional>
+                    <Conditional showWhen={node.su_publication_citation.su_month && !node.su_publication_citation.su_day}>
+                      {node.su_publication_citation.su_month}, {node.su_publication_citation.su_year}
+                    </Conditional>
+                    <Conditional showWhen={!node.su_publication_citation.su_month && !node.su_publication_citation.su_day}>
+                      {node.su_publication_citation.su_year}
+                    </Conditional>
+                  </div>
+                </Conditional>
+              </div>
+
+              <Conditional showWhen={node.su_publication_cta}>
+                <div className="su-rs-pl-3">
+                  <DrupalLinkButton href={node.su_publication_cta.url}>{node.su_publication_cta.title}</DrupalLinkButton>
+                </div>
+              </Conditional>
+
+            </div>
+        </div>
+        <div className="su-border-t su-border-black-10">
+          <h2 className="su-text-16 md:su-text-18 2xl:su-text-19 su-font-bold su-rs-pt-0">Related Topics</h2>
+          <div className="su-text-16 md:su-text-18 2xl:su-text-19 su-rs-mb-2">
+            {node.su_publication_topics && node.su_publication_topics.map((term, index) =>
+              <span key={term.id} className="su-text-digital-red su-text-16 md:su-text-18 2xl:su-text-19 su-rs-mb-2">{(index ? ', ' : '') + term.name}</span>
+            )}
           </div>
         </div>
-{/* 
-        {node.su_publication_topics && node.su_publication_topics.map(term=>
-          <div key={term.id} className="su-text-digital-red su-type-0">{term.name}</div>
-        )}
-
-        {node.su_publication_components && <Rows rows={node.su_publication_components} rowField="su_pubs_components"/>}
-
-        {node.su_publication_citation && <Citation citation={node.su_publication_citation}/>}
-        {node.su_publication_cta &&
-            <Link href={node.su_publication_cta.url}>{node.su_publication_cta.title}</Link>} */}
       </article>
     </MainContentLayout>
   )
