f934069231835dec1ffc3062917b670aaf02b9c3
Mike Decker <pookmish@gmail.com>
2023-02-17 10:42:01 -0800

    fixed centered container on 2 and 3 column layouts

--- src/components/layouts/two-column.tsx
+++ src/components/layouts/two-column.tsx
@@ -13,7 +13,7 @@ export const TwoColumn = ({config, items}) => {
   }
 
   return (
-    <div className={`su-cc su-grid su-gap-2xl ${gridClass}`}>
+    <div className={`su-max-w-1500 su-mx-auto su-grid su-gap-2xl ${gridClass}`}>
       <div className="su-min-w-0 su-grid su-grid-rows-1 su-gap-2xl">
         {leftItems.map(item => <Paragraph key={item.id} paragraph={item} siblingCount={9}/>)}
       </div>
