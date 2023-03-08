c3f5052f58dd270ab8f0673b1c7ac58c9bc6c897
Mike Decker <pookmish@gmail.com>
2023-03-07 17:07:07 -0800

    Style mobile side nav

--- app/components/menu/secondary-menu.tsx
+++ app/components/menu/secondary-menu.tsx
@@ -7,10 +7,14 @@ import {ChevronDownIcon} from "@heroicons/react/20/solid";
 import useActiveTrail from "@/lib/hooks/useActiveTrail";
 import {useIsDesktop} from "@/lib/hooks/useIsDesktop";
 import Conditional from "../utils/conditional";
+import OutsideClickHandler from "@/components/utils/outside-click-handler";
+import {useAutoAnimate} from "@formkit/auto-animate/react";
 
 const SecondaryMenu = ({menuItems}) => {
+  const [menuOpen, setMenuOpen] = useState(false)
   const activeTrail = useActiveTrail(menuItems);
   const isDesktop = useIsDesktop()
+  const [animationParent] = useAutoAnimate()
 
   // Peel off the menu items from the parent.
   const topMenuItem = activeTrail.length > 0 ? menuItems.find(item => item.id === activeTrail[0]) : false;
@@ -20,13 +24,54 @@ const SecondaryMenu = ({menuItems}) => {
     return null;
   }
 
+  const closeMobileMenu = () => {
+    setMenuOpen(false)
+  }
+
+  const getCurrentPageTitle = (menu) => {
+    for (let i = 0; i < menu.length; i++) {
+      if (menu[i].id === activeTrail.at(-1)) {
+        return menu[i].title
+      }
+      if (menu[i].items && menu[i].items.length > 0) {
+        return getCurrentPageTitle(menu[i].items)
+      }
+    }
+  }
+  const currentPageTitle = getCurrentPageTitle(menuItems);
+
   return (
     <aside className="lg:su-w-4/12">
-      <nav>
-        <ul className="su-list-unstyled su-py-20 su-mb-20 su-shadow-lg su-border su-border-t-8 su-border-archway">
-          {subTree.map(item => <SideMenuItem key={item.id} activeTrail={activeTrail} {...item}/>)}
-        </ul>
-      </nav>
+      {(menuOpen && !isDesktop) &&
+          <div className="su-backdrop-blur-sm su-fixed su-z-10 su-top-0 su-left-0 su-w-full su-h-screen">
+          </div>
+      }
+
+      <OutsideClickHandler onClickOutside={closeMobileMenu} onFocusOutside={closeMobileMenu}>
+        <nav ref={animationParent} className="su-relative">
+          {!isDesktop &&
+              <button
+                  onClick={() => setMenuOpen(!menuOpen)}
+                  className="su-block su-w-2/3 su-mx-auto su-flex su-mb-20 su-border su-border-t-8 su-border-archway su-bg-foggy-light su-text-archway-light"
+                  aria-haspopup="true"
+                  aria-expanded={menuOpen ? "true" : "false"}
+              >
+                <div aria-hidden={true} className="su-p-20 su-flex-grow su-font-semibold su-relative su-text-left">
+                  {currentPageTitle}
+                </div>
+                <ChevronDownIcon width={40} className="su-mr-20"/>
+
+                <span className="su-sr-only">{menuOpen ? 'Close' : 'Open'} Side Navigation</span>
+              </button>
+          }
+
+          {(isDesktop || menuOpen) &&
+              <ul className="su-absolute su-z-40 lg:su-relative su-top-0 su-left-0 su-w-full su-bg-white su-list-unstyled su-py-20 su-mb-20 su-shadow-lg su-border su-border-t-8 su-border-archway">
+                {subTree.map(item => <SideMenuItem key={item.id} activeTrail={activeTrail} {...item}/>)}
+              </ul>
+          }
+        </nav>
+      </OutsideClickHandler>
     </aside>
   )
 }
--- app/components/node/stanford-person/card.tsx
+++ app/components/node/stanford-person/card.tsx
@@ -4,13 +4,12 @@ import VerticalPersonCard from "./vertical-card";
 import HorizontalPersonCard from "./horizontal-card";
 import {useResizeDetector} from "react-resize-detector";
 
-
 const StanfordPersonCard = ({node, ...props}: { node: Person }) => {
   const {width, ref} = useResizeDetector();
-  const Card = (width && width > 510) ? HorizontalPersonCard : VerticalPersonCard;
+  const Card = (width && width < 510) ? VerticalPersonCard : HorizontalPersonCard;
   return (
     <div ref={ref} {...props}>
-      <Card node={node} {...props}/>
+      <Card node={node} currentWidth={width ?? 0} {...props}/>
     </div>
   )
 }
