66ee101704e46842aa65048ad6ab178be2c9bdbe
jdwjdwjdw <jacobdeanwilson@gmail.com>
2023-03-16 00:11:55 +0000

    Update places to study card structure and styles

--- app/(public)/components/node/sul-study-place/page-display.tsx
+++ app/(public)/components/node/sul-study-place/page-display.tsx
@@ -1,9 +1,11 @@
 import {StudyPlace} from "@/lib/drupal/drupal";
 
 const SulStudyPlace = ({node, ...props}: { node: StudyPlace }) => {
+  console.log(node)
 
   return (
     <>
+      {console.log('node: ', node)}
       Course
     </>
   )
