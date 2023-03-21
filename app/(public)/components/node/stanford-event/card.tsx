8fcfb5c442986373cc297785736fc5f7f5ba89e4
Mike Decker <pookmish@stanford.edu>
2023-03-21 17:40:49 +0000

    Fixed timezone issues

--- app/(public)/components/node/stanford-event/card.tsx
+++ app/(public)/components/node/stanford-event/card.tsx
@@ -5,59 +5,18 @@ import {Event} from "@/lib/drupal/drupal";
 import Image from "next/image";
 import {ClockIcon, MapPinIcon} from "@heroicons/react/24/outline";
 
-const StanfordEventCard = ({node, ...props}: { node: Event }) => {
-  const start = new Date(node.su_event_date_time?.value);
-  const end = new Date(node.su_event_date_time?.end_value);
-  console.log(node.su_event_date_time?.value);
 
-  const shortMonth = start.toLocaleDateString("en-US", {
-    month: "short",
-    timeZone: 'America/Los_Angeles'
-  })
-  const day = start.toLocaleDateString("en-US", {
-    day: "numeric",
-    timeZone: 'America/Los_Angeles'
-  })
-  const startDay = start.toLocaleDateString("en-US", {timeZone: 'America/Los_Angeles'})
+const getTimeString = (start: Date, end: Date): string => {
+  const startHour = parseInt(start.toLocaleTimeString("en-US", {hour: "numeric", hour12: false, timeZone: 'America/Los_Angeles'}))
+  const startMinute = parseInt(start.toLocaleTimeString("en-US", {minute: "numeric", hour12: false, timeZone: 'America/Los_Angeles'}))
 
-  let dateTimeString = "";
+  const endHour = parseInt(end.toLocaleTimeString("en-US", {hour: "numeric", hour12: false, timeZone: 'America/Los_Angeles'}))
+  const endMinute = parseInt(end.toLocaleTimeString("en-US", {minute: "numeric", hour12: false, timeZone: 'America/Los_Angeles'}))
 
-  // Same Day Display
-  if (start.getMonth() === end.getMonth() && start.getDate() === end.getDate() && start.getFullYear() === end.getFullYear()) {
-    // Not ALl Day.
-    if (
-      start.getHours() !== 0 ||
-      start.getMinutes() !== 0 ||
-      end.getHours() !== 23 ||
-      end.getMinutes() !== 59
-    ) {
+  let dateTimeString;
 
-      if (start.getHours() !== end.getHours() || start.getMinutes() !== end.getMinutes()) {
-        dateTimeString += start.toLocaleTimeString("en-US", {
-          hour: "numeric",
-          minute: "numeric",
-          timeZone: 'America/Los_Angeles'
-        });
-        dateTimeString += ' - ';
-        dateTimeString += end.toLocaleTimeString("en-US", {
-          hour: "numeric",
-          minute: "numeric",
-          timeZoneName: "short",
-          timeZone: 'America/Los_Angeles'
-        })
-      } else {
-        dateTimeString += ' ' + start.toLocaleTimeString("en-US", {
-          hour: "numeric",
-          minute: "numeric",
-          timeZoneName: "short",
-          timeZone: 'America/Los_Angeles'
-        })
-      }
-    }else {
-      dateTimeString = 'All Day';
-    }
-  } else {
-    // Multiple days
+  // Multiple days.
+  if(start.toLocaleDateString("en-US", {timeZone: 'America/Los_Angeles'}) != end.toLocaleDateString("en-US", {timeZone: 'America/Los_Angeles'})){
     dateTimeString = start.toLocaleDateString("en-US", {
       month: "long",
       day: "numeric",
@@ -69,36 +28,75 @@ const StanfordEventCard = ({node, ...props}: { node: Event }) => {
       year: "numeric",
       timeZone: 'America/Los_Angeles'
     })
+    return dateTimeString;
   }
+
+  // All Day display.
+  if (
+    startHour === 24 &&
+    startMinute === 0 &&
+    endHour === 23 &&
+    endMinute === 59
+  ) {
+    return 'All Day';
+  }
+  
+
+  // Different start and end times.
+  if (startHour !== endHour || startMinute !== endMinute) {
+    dateTimeString = start.toLocaleTimeString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      timeZone: 'America/Los_Angeles'
+    });
+    dateTimeString += ' - ';
+    dateTimeString += end.toLocaleTimeString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      timeZoneName: "short",
+      timeZone: 'America/Los_Angeles'
+    })
+    return dateTimeString;
+  } 
+
+  // Start and end times are the same, just display the start time.
+  return start.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    timeZoneName: "short",
+    timeZone: 'America/Los_Angeles'
+  })
+}
+
+const StanfordEventCard = ({node, ...props}: { node: Event }) => {
+  const start = new Date(node.su_event_date_time?.value);
+  const end = new Date(node.su_event_date_time?.end_value);
+  const startMonth = start.toLocaleDateString("en-US", {month: "short", timeZone: 'America/Los_Angeles'})
+  const startDay = parseInt(start.toLocaleDateString("en-US", {day: "numeric", timeZone: 'America/Los_Angeles'}))
 
   // Fix difference between server side render and client side render. Replace any strange characters.
-  dateTimeString = dateTimeString.replace(/[^a-zA-Z0-9 ,:\-|]/, ' ');
+  const dateTimeString = getTimeString(start, end).replace(/[^a-zA-Z0-9 ,:\-|]/, ' ');;
 
-
-  let image;
   let imageUrl = node.sul_event__image?.field_media_image?.image_style_uri?.breakpoint_2xl_1x;
-  if (imageUrl) {
-    image = <Image
-      className="su-object-cover su-object-center"
-      src={imageUrl}
-      alt=""
-      fill={true}
-    />
-  }
 
   return (
     <article {...props}>
-      {image &&
+      {imageUrl &&
           <div className="su-overflow-hidden su-aspect-[4/3] su-relative su-mb-40" aria-hidden="true">
-            {image}
+            <Image
+              className="su-object-cover su-object-center"
+              src={imageUrl}
+              alt=""
+              fill={true}
+            />
           </div>
       }
 
       <div className="su-flex su-gap-xl">
         <div>
           <div
-            className="su-bg-black-true su-text-white su-pt-20 su-px-30 su-text-center su-uppercase">{shortMonth}</div>
-          <div className="su-bg-black-true su-text-white su-pb-20 su-px-30 su-text-center su-text-m4">{day}</div>
+            className="su-bg-black-true su-text-white su-pt-20 su-px-30 su-text-center su-uppercase">{startMonth}</div>
+          <div className="su-bg-black-true su-text-white su-pb-20 su-px-30 su-text-center su-text-m4">{startDay}</div>
         </div>
 
         <div>
@@ -118,7 +116,7 @@ const StanfordEventCard = ({node, ...props}: { node: Event }) => {
 
           <div className="su-flex su-mb-20">
             <CalendarDaysIcon width={20} className="su-mr-20"/>
-            {startDay}
+            {start.toLocaleDateString("en-US", {timeZone: 'America/Los_Angeles'})}
           </div>
 
           <div className="su-flex su-mb-20">
