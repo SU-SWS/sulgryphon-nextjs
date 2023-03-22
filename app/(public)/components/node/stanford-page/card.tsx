2e3e753a44980c8a578480e51f5d99631290e674
Mike Decker <pookmish@gmail.com>
2023-03-22 10:35:12 -0700

    SUL23-194 Style basic page card display to match designs

--- app/(public)/components/node/stanford-page/card.tsx
+++ app/(public)/components/node/stanford-page/card.tsx
@@ -1,47 +1,32 @@
+"use client"
 import Image from "next/image";
-import {useMemo} from "react";
 import Link from "next/link";
 import {BasicPage} from "@/lib/drupal/drupal";
-import Card from "../../patterns/card";
 
 const StanfordPageCard = ({node, ...props}: { node: BasicPage }) => {
-
-  const getFeaturedImageUrl = (node: BasicPage, imageStyle = 'breakpoint_2xl_1x'): string => {
-    if (node.su_page_image?.field_media_image?.image_style_uri?.[imageStyle]) {
-      return node.su_page_image.field_media_image.image_style_uri?.[imageStyle]
-    }
-
-    if (node.su_page_banner?.su_banner_image?.field_media_image?.image_style_uri?.[imageStyle]) {
-      return node.su_page_banner.su_banner_image.field_media_image.image_style_uri?.[imageStyle];
-    }
-    return '';
-  }
-
-  const getImage = () => {
-    let imageUrl = getFeaturedImageUrl(node);
-    if (imageUrl) {
-      return <Image
-        className="su-object-cover su-object-center"
-        src={imageUrl}
-        alt=""
-        fill={true}
-      />
-    }
-  }
-
-  const image = useMemo(() => getImage(), [node]);
-
+  const imageUrl = node.su_page_image?.field_media_image?.image_style_uri?.breakpoint_2xl_1x || node.su_page_banner?.su_banner_image?.field_media_image?.image_style_uri?.breakpoint_2xl_1x;
   return (
     <article {...props}>
-      <Card
-        image={image}
-        header={
-          <Link className="su-underline hocus:su-no-underline active:su-no-underline su-text-black hocus:su-text-brick-dark active:su-text-digital-red" href={node.path.alias}>
-            {node.title}
-          </Link>
-        }
-        body={node.su_page_description}
-      />
+      {imageUrl &&
+          <div className="su-overflow-hidden su-aspect-[16/9] su-relative su-mb-30" aria-hidden="true">
+            <Image
+                className="su-object-cover su-object-center"
+                src={imageUrl}
+                alt=""
+                fill={true}
+            />
+          </div>
+      }
+
+      <h2 className="su-text-m2 su-mb-20">
+        <Link
+          className="su-underline hocus:su-no-underline active:su-no-underline su-text-black hocus:su-text-brick-dark active:su-text-digital-red"
+          href={node.path.alias}>
+          {node.title}
+        </Link>
+      </h2>
+
+      <p>{node.su_page_description}</p>
     </article>
   )
 }
