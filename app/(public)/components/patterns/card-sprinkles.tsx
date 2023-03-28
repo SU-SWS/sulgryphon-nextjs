833e3e1d77c37b50d1ae1cd3f3d9b7be5f60dce4
Marc Young <mdyoung3@stanford.edu>
2023-03-28 12:27:07 -0600

    sul23-115 - tweak bg top right image.

--- app/(public)/components/patterns/card-sprinkles.tsx
+++ app/(public)/components/patterns/card-sprinkles.tsx
@@ -1,7 +1,6 @@
 export const TopRightSprinkles = (props) => {
   return (
     <div className={ `su-w-2/3 su-h-2/3 su-absolute su-top-[-26px] su-right-0 su-bg-no-repeat su-bg-card-top-right-sprinkles su-bg-right` }>
-
     </div>
   )
 }
@@ -9,7 +8,6 @@ export const TopRightSprinkles = (props) => {
 export const TopLeftSprinkles = (props) => {
   return (
     <div className={ `su-w-2/3 su-h-2/3 su-absolute su-top-[-26px] su-left-0 su-bg-no-repeat su-bg-card-top-left-sprinkles` }>
-
     </div>
   )
 }
@@ -17,7 +15,6 @@ export const TopLeftSprinkles = (props) => {
 export const BottomLeftSprinkles = (props) => {
   return (
     <div className={ `md:su-w-[563px] sm:su-w-[335px] su-w-[235px] su-h-[333px] su-absolute su-bottom-0 su-left-0 su-bg-no-repeat su-bg-card-bottom-left-sprinkles su-bg-contain su-bg-bottom` }>
-
     </div>
   )
 }
@@ -25,7 +22,6 @@ export const BottomLeftSprinkles = (props) => {
 export const BottomRightSprinkles = (props) => {
   return (
     <div className={ `md:su-w-[563px] sm:su-w-[335px] su-w-[235px] su-h-[333px] su-absolute su-bottom-0 su-right-0 su-bg-no-repeat su-bg-card-bottom-right-sprinkles su-bg-contain su-bg-bottom` }>
-
     </div>
   )
 }
