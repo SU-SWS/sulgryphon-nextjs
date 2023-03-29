fb5a669b47c9c2ed87549709d5211808efd3773b
Mike Decker <pookmish@gmail.com>
2023-03-29 11:56:07 -0700

    Fixed views arguments when fetching

--- app/(public)/api/views/[...slug]/route.tsx
+++ app/(public)/api/views/[...slug]/route.tsx
@@ -4,15 +4,14 @@ import {getResource} from "@/lib/drupal/get-resource";
 import {getView} from "@/lib/drupal/get-view";
 import {NextRequest, NextResponse} from "next/server";
 
-export const GET = async (request: NextRequest, { params }) => {
+export const GET = async (request: NextRequest, {params}) => {
   const [viewId, displayId, options] = params.slug
   let [args, itemsToDisplay] = options.split(":")
   const drupalParams = new DrupalJsonApiParams();
 
-  if (args.length >= 1) {
-    args += '/0/0/0/0/0/0';
-    drupalParams.addCustomParam({'views-argument': args.split('/')});
-  }
+  args += '/0/0/0/0/0/0';
+  drupalParams.addCustomParam({'views-argument': args.replace(/^\//, '').split('/')});
+
   if (itemsToDisplay) {
     drupalParams.addPageLimit(itemsToDisplay);
   }
