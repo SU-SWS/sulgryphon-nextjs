1bfae2b1cd7da1057bdad2d56dd2bac6e94e0379
jdwjdwjdw <jacobdeanwilson@gmail.com>
2023-03-31 20:50:08 +0000

    Update Place of Study list and page display, remove console logs

--- app/(public)/components/node/sul-study-place/list-item.tsx
+++ app/(public)/components/node/sul-study-place/list-item.tsx
@@ -1,11 +1,8 @@
 import {StudyPlace} from "@/lib/drupal/drupal";
+import SulStudyPlaceCard from "./card";
 
-const SulStudyPlaceListItem = ({node}: { node: StudyPlace }) => {
-  return (
-    <>
-      {console.log('node 2: ', node)}
-      ListItem
-    </>
-  )
+const SulStudyPlaceListItem = ({node, ...props}: { node: StudyPlace }) => {
+  // For now, just return the card. Change this if designs come through.
+  return <SulStudyPlaceCard node={node} {...props}/>
 }
 export default SulStudyPlaceListItem;
