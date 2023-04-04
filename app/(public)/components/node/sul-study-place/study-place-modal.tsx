9fe69db6c7b89d638ea84c6049440d55ea00e29c
jdwjdwjdw <jacobdeanwilson@gmail.com>
2023-04-04 22:31:45 +0000

    Update Modal Props

--- app/(public)/components/node/sul-study-place/study-place-modal.tsx
+++ app/(public)/components/node/sul-study-place/study-place-modal.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import {useState} from "react";
-import {StudyPlace} from "@/lib/drupal/drupal";
+import {PropsWithoutRef, useState} from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Modal from "../../patterns/modal";
@@ -12,12 +11,21 @@ import LibCal from "./libcal";
 import StudyPlaceHours from "./study-place-today-hours";
 import Conditional from "@/components/utils/conditional";
 
-const StudyPlaceModal = ({node}: { node: StudyPlace }) => {
+ interface ModalProps extends PropsWithoutRef<any> {
+  branchHours?: string
+  branchTitle: string
+  branchUrl: string
+  capacity?: string
+  contactImageAlt: string
+  contactImageUrl: string
+  features?: {id: string, name: string}[]
+  libCal?: string
+  type: string
+ }
+
+const StudyPlaceModal = ({branchHours, branchTitle, branchUrl, capacity, contactImageAlt, contactImageUrl, features, libCal, type}: ModalProps) => {
   const [modalOpen, setModalOpen] = useState(false);
 
-  const contactImageUrl = node.sul_study__branch.su_library__contact_img?.field_media_image?.image_style_uri?.breakpoint_md_2x
-
-  const features = node.sul_study__features?.filter(feature => feature.name?.length > 0) ?? [];
   return (
     <ErrorBoundary
       fallback={<></>}
@@ -32,7 +40,7 @@ const StudyPlaceModal = ({node}: { node: StudyPlace }) => {
         isOpen={modalOpen}
         onClose={() => setModalOpen(false)}
         ariaLabel="Features Modal"
-      >
+        >
         <div className={"su-bg-white su-flex su-w-full su-leading-display su-shadow-md su-border-0 su-rounded su-flex-row"}>
           <div className="su-hidden md:su-block su-rs-px-3 su-rs-py-3 su-w-1/2">
             {contactImageUrl &&
@@ -40,40 +48,40 @@ const StudyPlaceModal = ({node}: { node: StudyPlace }) => {
                 <Image
                   className="su-object-cover su-object-center su-static"
                   src={contactImageUrl}
-                  alt={node.sul_study__branch.su_library__contact_img?.field_media_image?.resourceIdObjMeta?.alt ?? ''}
+                  alt={contactImageAlt}
                   fill={true}
                 />
               </div>
             }
 
-            <Conditional showWhen={node.sul_study__libcal_id}>
-              <LibCal libcalId={node.sul_study__libcal_id}/>
+            <Conditional showWhen={libCal}>
+              <LibCal libcalId={libCal}/>
             </Conditional>
           </div>
 
           <div className="card-body su-items-start su-rs-px-3 su-rs-pb-3 su-rs-pt-7 md:su-rs-pt-3 su-w-full">
             <div className="su-leading-display su-text-18 su-pt-0 su-font-normal ">
-              <h2 className="su-type-3 su-rs-mb-1">{node.sul_study__type.name}</h2>
+              <h2 className="su-type-3 su-rs-mb-1">{type}</h2>
               <div className="su-leading-tight">
 
-                {node.sul_study__branch?.su_library__hours &&
-                  <StudyPlaceHours hoursId={node.sul_study__branch.su_library__hours}/>
+                {branchHours &&
+                  <StudyPlaceHours hoursId={branchHours}/>
                 }
 
                 <div className="su-relative su-flex su-flex-row su-items-start su-type-1 su-rs-mb-2">
                   <MapPinIcon width={19} className="su-mt-01em md:su-mt-0 su-mr-12 su-flex-shrink-0"/>
-                  <Link href={node.sul_study__branch?.path.alias} className="su-transition-colors hover:su-text-brick-dark hover:su-bg-black-10 hover:su-no-underline focus:su-bg-none focus:su-text-cardinal-red active:su-text-cardinal-red">
-                    <div>{node.title}</div>
+                  <Link href={branchUrl} className="su-transition-colors hover:su-text-brick-dark hover:su-bg-black-10 hover:su-no-underline focus:su-bg-none focus:su-text-cardinal-red active:su-text-cardinal-red">
+                    <div>{branchTitle}</div>
                   </Link>	
                 </div>
 
-                {(node.sul_study__capacity || features) &&
+                {(capacity || features) &&
                   <ul className="su-ml-10 su-rs-mb-1">
-                    {node.sul_study__capacity &&
-                      <li className="su-type-1 su-leading-display">{node.sul_study__capacity.name}</li>
+                    {capacity &&
+                      <li className="su-type-1 su-leading-display">{capacity}</li>
                     }
                     {features && features.map(feature =>
-                      <li key={`feature-${feature.id}`} className="su-type-1 su-leading-display">
+                      <li key={`modal-feature-${feature.id}`} className="su-type-1 su-leading-display">
                         {feature.name}
                       </li>
                     )}
