9220c879e7bc02ec79a36c7f365f2ef215e0bd90
jdwjdwjdw <jacobdeanwilson@gmail.com>
2023-04-04 22:31:45 +0000

    Update Modal Props

--- app/(public)/components/node/sul-study-place/card.tsx
+++ app/(public)/components/node/sul-study-place/card.tsx
@@ -60,7 +60,17 @@ const SulStudyPlaceCard = ({node}: { node: StudyPlace }) => {
               }
 
               {(features && features.length > 4) &&
-                <StudyPlaceModal node={node} />
+                <StudyPlaceModal
+                  branchHours={node.sul_study__branch?.su_library__hours}
+                  branchTitle={node.sul_study__branch.title}
+                  branchUrl={node.sul_study__branch.path.alias}
+                  capacity={node.sul_study__capacity?.name}
+                  contactImageAlt={node.sul_study__branch.su_library__contact_img?.field_media_image?.resourceIdObjMeta?.alt ?? ''}
+                  contactImageUrl={node.sul_study__branch.su_library__contact_img?.field_media_image?.image_style_uri?.breakpoint_md_2x}
+                  features={features.map(feature => ({id: feature.id, name: feature.name}))}
+                  libCal={node.sul_study__libcal_id}
+                  type={node.sul_study__type.name}
+                />
               }
 
             </div>
