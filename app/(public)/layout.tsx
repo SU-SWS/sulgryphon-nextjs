edbd09a13e53d04e6f275453df3f8824a938c4c6
Mike Decker <pookmish@gmail.com>
2023-05-01 16:57:59 -0700

    debug

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -2,36 +2,26 @@ import LibraryFooter from "@/components/layout/library-footer";
 import GlobalFooter from "@/components/layout/global-footer";
 import Header from "@/components/layout/header";
 import {ReactNode} from "react";
-import Script from "next/script";
-import GoogleAnalytics from "@/components/utils/google-analytics";
-
 
 const Layout = ({children}: { children: ReactNode }) => {
 
   return (
-    <>
-      {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID &&
-        <>
-          <Script async src="//siteimproveanalytics.com/js/siteanalyze_80352.js"/>
-          <GoogleAnalytics/>
-        </>
-      }
-      <div className="su-grid su-grid-rows-1 su-min-h-screen">
-        <div>
-          {/* @ts-expect-error Async Server Component */}
-          <Header/>
 
-          <div className="su-mb-50">
-            {children}
-          </div>
+    <div className="su-grid su-grid-rows-1 su-min-h-screen">
+      <div>
+        {/* @ts-expect-error Async Server Component */}
+        <Header/>
+
+        <div className="su-mb-50">
+          {children}
         </div>
-
-        <footer className="su-row-start-2 su-row-end-3">
-          <LibraryFooter/>
-          <GlobalFooter/>
-        </footer>
       </div>
-    </>
+
+      <footer className="su-row-start-2 su-row-end-3">
+        <LibraryFooter/>
+        <GlobalFooter/>
+      </footer>
+    </div>
   )
 }
 export default Layout;
--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,18 +1,11 @@
 import {getResourceByPath} from "next-drupal/src/get-resource";
 import {BasicPage} from "@/lib/drupal/drupal";
-import {Metadata} from "next";
-import {getNodeMetadata} from "./[...slug]/metadata";
 import HomePageBanner from "@/components/node/stanford-page/home-page/home-page-banner";
 import {ParagraphRows} from "@/components/paragraph/rows/rows";
 import fetchComponents from "@/lib/fetch-components";
 import {DrupalParagraph} from "next-drupal";
 
 export const revalidate = 60;
-
-export const generateMetadata = async (): Promise<Metadata> => {
-  const node: BasicPage = await getResourceByPath('/');
-  return getNodeMetadata(node);
-}
 
 const Page = async () => {
   const node: BasicPage = await getResourceByPath('/');
