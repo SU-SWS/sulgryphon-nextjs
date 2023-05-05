a95977b2d91009a09729fcd2b3ffa36aa240c5d2
Mike Decker <pookmish@gmail.com>
2023-05-04 22:00:01 -0700

    fixed gutters on 2 & 3 column rows

--- components/paragraph/rows/three-column.tsx
+++ components/paragraph/rows/three-column.tsx
@@ -7,13 +7,13 @@ interface LayoutProps {
   config: {}
 }
 
-const ThreeColumn = ({items, config = {}}: LayoutProps) => {
+const ThreeColumn = ({items,fullWidth, config = {}}: LayoutProps) => {
   const leftItems = items.filter(item => item.behavior_settings.layout_paragraphs.region === 'left');
   const mainItems = items.filter(item => item.behavior_settings.layout_paragraphs.region === 'main');
   const rightItems = items.filter(item => item.behavior_settings.layout_paragraphs.region === 'right');
   return (
     <div data-rows="three-column"
-         className="su-max-w-1500 su-w-full su-mx-auto su-grid su-gap-2xl lg:su-grid-cols-3 su-px-40 3xl:su-px-0">
+         className={"su-max-w-1500 su-w-full su-mx-auto su-grid su-px-40 3xl:su-px-0" + (fullWidth ? " su-px-40 3xl:su-px-0": "")}>
       <div className="su-relative su-min-w-0 su-grid su-grid-rows-1 su-gap-2xl">
         {leftItems.map(item => <Paragraph key={item.id} paragraph={item}/>)}
       </div>
