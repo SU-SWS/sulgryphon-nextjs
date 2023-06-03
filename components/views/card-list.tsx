be1521526e313414df4dd6239c7cce811c6f7965
Mike Decker <pookmish@gmail.com>
2023-06-02 22:14:48 -0700

    fix filter

--- components/views/card-list.tsx
+++ components/views/card-list.tsx
@@ -12,7 +12,7 @@ const CardList = ({items, h3Heading}) => {
   return (
     <div className="su-@container">
       <ul className={`su-list-unstyled su-grid su-gap-[90px] ${gridClass}`}>
-        {items.filter(item => item.id).map(item =>
+        {items.filter(item => !!item?.id).map(item =>
           <li key={item.id} className="su-w-full su-max-w-[500px] su-mx-auto">
             <NodeCard node={item} key={item.id} h3Heading={h3Heading}/>
           </li>
