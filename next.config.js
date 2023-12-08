353080aea670765c4266740bf2e696f3c9c0576a
Mike Decker <pookmish@stanford.edu>
2023-12-08 12:54:27 -0800

    Fixed rewrites for image

--- next.config.js
+++ next.config.js
@@ -16,23 +16,6 @@ module.exports = {
   typescript: {
     ignoreBuildErrors: process.env.CI !== 'true',
   },
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/_next/image',
-          destination: '/_next/image?url=/no-image.png',
-          has: [{type: 'query', key: 'url', value: (`htt[p|ps]://${process.env.NEXT_IMAGE_DOMAIN}.*`)}],
-          missing: [{type: 'query', key: 'url', value: '(.*itok=([\\w|-]+))'}]
-        },
-        {
-          source: '/_next/image',
-          destination: '/_next/image?url=:url',
-          has: [{type: 'query', key: 'url', value: '(?<url>.*[jpg|png|jpeg|gif]\?itok=([\\w|-]+)).*'}]
-        },
-      ]
-    };
-  },
   async headers() {
     if (process.env.NEXT_PUBLIC_NOBOTS === 'true') {
       return [
@@ -72,5 +55,22 @@ module.exports = {
         permanent: true,
       }
     ]
-  }
+  },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/_next/image',
+          destination: '/_next/image?url=/no-image.png',
+          has: [{type: 'query', key: 'url', value: (`${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}.*`)}],
+          missing: [{type: 'query', key: 'url', value: '(.*itok=([\\w|-]+))'}]
+        },
+        {
+          source: '/_next/image',
+          destination: '/_next/image?url=:url',
+          has: [{type: 'query', key: 'url', value: '(?<url>.*[jpg|png|jpeg|gif]\?itok=([\\w|-]+)).*'}]
+        },
+      ]
+    };
+  },
 }
