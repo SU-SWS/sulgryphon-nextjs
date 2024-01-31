2b1e1e34b1fd085af9275d42d961c13c1a70485e
Mike Decker <pookmish@gmail.com>
2024-01-30 16:23:43 -0800

    Disable link prefetching

--- src/components/patterns/elements/drupal-link.tsx
+++ src/components/patterns/elements/drupal-link.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import {HTMLAttributes} from "react";
+
 type Props = HTMLAttributes<HTMLAnchorElement> & {
   href: string
+  prefetch?: boolean
 }
-const DrupalLink = ({href, children, ...props}: Props) => {
+const DrupalLink = ({href, children, prefetch, ...props}: Props) => {
   href = href.replace(process.env.NEXT_PUBLIC_DRUPAL_BASE_URL as string, '');
-  const prefetch = href.startsWith('/') || href.startsWith('https://library.stanford.edu');
+
   return (
-    <Link href={href} prefetch={prefetch} rel={prefetch ? undefined : "nofollow"} {...props}>
+    <Link href={href} prefetch={prefetch || false} rel={prefetch ? undefined : "nofollow"} {...props}>
       {children}
     </Link>
   )
