ad23c0b8d6c5f964dc9fb15eea379f4311be83e9
Marc Young <mdyoung3@stanford.edu>
2024-02-16 16:18:19 -0700

    Adding space.

--- src/components/patterns/elements/select-list.tsx
+++ src/components/patterns/elements/select-list.tsx
@@ -129,7 +129,7 @@ const SelectList = ({options, label, multiple, ariaLabelledby, ...props}: Props)
       >
         <div className="flex flex-wrap">
           {label &&
-            <div className={"relative p-10" + (optionChosen ? "top-[-15px] w-full" : "")}>
+            <div className={"relative p-10 " + (optionChosen ? "top-[-15px] w-full" : "")}>
               <div id={labelId} className="bg-white w-fit px-4">
                 {label}
               </div>
