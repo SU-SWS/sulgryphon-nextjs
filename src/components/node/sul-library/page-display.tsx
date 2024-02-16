81f8581cb5121686b2c90df8f3bcd24f72bca9f2
Marc Young <mdyoung3@stanford.edu>
2024-02-16 11:10:20 -0700

    Feedback

--- src/components/node/sul-library/page-display.tsx
+++ src/components/node/sul-library/page-display.tsx
@@ -14,20 +14,20 @@ const SulLibrary = async ({node, ...props}: { node: Library }) => {
   return (
     <article className="mb-50 @container" {...props}>
       {node.su_library__hours &&
-        <div className={containerStyles}>
+        <div className="mb-50 lg:max-w-[980px] mx-auto centered">
           <LibraryAdditionalHours hoursId={node.su_library__hours}/>
         </div>
       }
 
-      {<ParagraphRows items={node.su_library__paragraphs} fullWidth={fullWidth}/>}
+      <ParagraphRows items={node.su_library__paragraphs} fullWidth={fullWidth}/>
 
       {node.sul_library__a11y &&
-            <div className={containerStyles}>
-              <div className="shadow-md py-20 px-30 w-fit mx-auto border border-black-10">
-                <h2 className="text-m3">Accessibility</h2>
-                {formatHtml(node.sul_library__a11y)}
-              </div>
-            </div>
+        <div  className="mb-50 lg:max-w-[980px] mx-auto centered">
+          <div className="shadow-md py-20 px-30 w-fit mx-auto border border-black-10">
+            <h2 className="text-m3">Accessibility</h2>
+            {formatHtml(node.sul_library__a11y)}
+          </div>
+        </div>
       }
 
     </article>
