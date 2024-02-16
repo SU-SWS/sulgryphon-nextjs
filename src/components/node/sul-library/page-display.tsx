2a8c43af1819e434966b5f1a1becba5ffdc28a0f
Marc Young <mdyoung3@stanford.edu>
2024-02-16 12:04:55 -0700

    Sul23-418 accessibility additional hour element (#109)

--- src/components/node/sul-library/page-display.tsx
+++ src/components/node/sul-library/page-display.tsx
@@ -11,26 +11,21 @@ const SulLibrary = async ({node, ...props}: { node: Library }) => {
 
   return (
     <article className="mb-50 @container" {...props}>
-
-      {(node.sul_library__a11y || node.su_library__hours) &&
-        <div
-          className="centered mb-50 flex flex-col @6xl:flex-row gap-[90px]">
-          {node.sul_library__a11y &&
-            <div className="order-last @6xl:order-first flex-1 basis-1/2">
-              <div className="shadow-md py-20 px-30 w-fit mx-auto border border-black-10">
-                <h2 className="text-m3">Accessibility</h2>
-                {formatHtml(node.sul_library__a11y)}
-              </div>
-            </div>
-          }
-
-          {node.su_library__hours &&
-            <LibraryAdditionalHours hoursId={node.su_library__hours}/>
-          }
+      {node.su_library__hours &&
+        <div className="mb-50 lg:max-w-[980px] w-full mx-auto centered">
+          <LibraryAdditionalHours hoursId={node.su_library__hours}/>
         </div>
       }
 
       <ParagraphRows items={node.su_library__paragraphs} fullWidth={fullWidth}/>
+
+      {node.sul_library__a11y &&
+        <div  className="mb-50 lg:max-w-[980px] w-full mx-auto centered shadow-md py-20 px-30 border border-black-10">
+          <h2 className="text-m3">Accessibility</h2>
+          {formatHtml(node.sul_library__a11y)}
+        </div>
+      }
+
     </article>
   )
 }
