9d48e41c3a0bdd4f8a61efc1ae9ec2a957bad7cd
Mike Decker <pookmish@gmail.com>
2024-02-29 21:33:47 -0800

    Replaced person card size observer

--- src/components/node/stanford-person/card.tsx
+++ src/components/node/stanford-person/card.tsx
@@ -2,11 +2,17 @@
 
 import VerticalPersonCard from "./vertical-card";
 import HorizontalPersonCard from "./horizontal-card";
-import {useResizeDetector} from "react-resize-detector";
 import {NodeStanfordPerson} from "@/lib/gql/__generated__/drupal.d";
+import {useDebounceCallback, useResizeObserver} from "usehooks-ts";
+import {useRef, useState} from "react";
 
 const StanfordPersonCard = ({node, ...props}: { node: NodeStanfordPerson }) => {
-  const {width, ref} = useResizeDetector();
+  const ref = useRef<HTMLDivElement>(null)
+  const [{width}, setSize] = useState<{ width?: number, height?: number }>({width: undefined})
+
+  const onResize = useDebounceCallback(setSize, 200)
+  useResizeObserver({ref, onResize})
+
   const Card = (width && width < 510) ? VerticalPersonCard : HorizontalPersonCard;
   return (
     <div ref={ref}>
