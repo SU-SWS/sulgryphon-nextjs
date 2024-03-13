09e451c86d0f6457f947ab53927f0b963d55208a
Marc Young <marcdeeyoung@gmail.com>
2024-03-13 13:13:46 -0600

    Reduced by 20%

--- src/styles/tailwind/plugins/base/base.js
+++ src/styles/tailwind/plugins/base/base.js
@@ -5,50 +5,50 @@ module.exports = function () {
         fontFamily: '"Source Serif Pro", Georgia, Times, "Times New Roman", serif',
       },
       'h1': {
-        fontSize: '2.31em',
+        fontSize: '1.85em',
         letterSpacing: '-0.02em',
         '@screen md': {
-          fontSize: '2.69em',
+          fontSize: '2.39em',
         },
         '@screen lg': {
           fontSize: '3.81em',
         },
       },
       'h2': {
-        fontSize: '1.75em',
+        fontSize: '1.4em',
         letterSpacing: '-0.016em',
         '@screen md': {
-          fontSize: '1.86em',
+          fontSize: '1.66em',
         },
         '@screen lg': {
           fontSize: '2.44em',
         },
       },
       'h3': {
-        fontSize: '1.52em',
+        fontSize: '1.22em',
         letterSpacing: '-0.014em',
         '@screen md': {
-          fontSize: '1.56em',
+          fontSize: '1.38em',
         },
         '@screen lg': {
           fontSize: '1.95em',
         },
       },
       'h4': {
-        fontSize: '1.32em',
+        fontSize: '1.06em',
         letterSpacing: '-0.012em',
         '@screen md': {
-          fontSize: '1.3em',
+          fontSize: '1.15em',
         },
         '@screen lg': {
           fontSize: '1.56em',
         },
       },
       'h5': {
-        fontSize: '1.15em',
+        fontSize: '0.92em',
         letterSpacing: '-0.01em',
         '@screen md': {
-          fontSize: '1.08em',
+          fontSize: '0.96em',
         },
         '@screen lg': {
           fontSize: '1.25em',
