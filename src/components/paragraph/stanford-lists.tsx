12e3a650e2e15eefb64b296a5ed681318fce12d1
Mike Decker <pookmish@gmail.com>
2024-03-27 18:38:04 -0700

    Redirect /sites/ directory to home page

--- src/components/paragraph/stanford-lists.tsx
+++ src/components/paragraph/stanford-lists.tsx
@@ -75,8 +75,23 @@ const ListParagraph = async ({headerId, headline, description, link, view, behav
   )
 }
 
-const getViewItems = cache(async (viewId: string, displayId: string, contextualFilter?: ViewReference["contextualFilter"], pageSize?: Maybe<number>): Promise<NodeUnion[]> => {
+const getViewItems = cache(async (viewId: string, displayId: string, contextualFilter?: Maybe<string[]>, pageSize?: Maybe<number>): Promise<NodeUnion[]> => {
+  let results: NodeUnion[] = [];
+  let pageResults: NodeUnion[] = [];
+  const itemsPerPage = pageSize ? Math.max(3, Math.min(Math.ceil(pageSize / 3) * 3, 99)) : undefined;
+  let fetchMore = true;
+  let page = 0;
 
+  while (fetchMore) {
+    pageResults = await getViewPagedItems(viewId, displayId, contextualFilter, pageSize, page);
+    results = [...results, ...pageResults];
+    fetchMore = itemsPerPage === 99 && pageResults.length === itemsPerPage;
+    page++;
+  }
+  return results;
+})
+
+const getViewPagedItems = cache(async (viewId: string, displayId: string, contextualFilter?: Maybe<string[]>, pageSize?: Maybe<number>, page?: Maybe<number>, offset?: Maybe<number>): Promise<NodeUnion[]> => {
   let items: NodeUnion[] = []
 
   const tags = ['views'];
@@ -115,56 +130,57 @@ const getViewItems = cache(async (viewId: string, displayId: string, contextualF
   let filters = getViewFilters(['term_node_taxonomy_name_depth', 'nid'], contextualFilter)
   let graphqlResponse;
   const itemsPerPage = pageSize ? Math.min(Math.ceil(pageSize / 3) * 3, 99) : undefined;
+  const queryVariables = {pageSize: itemsPerPage, page, offset};
 
   switch (`${viewId}--${displayId}`) {
     case 'stanford_shared_tags--card_grid':
       filters = getViewFilters(['term_node_taxonomy_name_depth', 'type'], contextualFilter)
-      graphqlResponse = await client.stanfordSharedTags({filters, pageSize: itemsPerPage});
+      graphqlResponse = await client.stanfordSharedTags({filters, ...queryVariables});
       items = graphqlResponse.stanfordSharedTags?.results as unknown as NodeUnion[]
       break
 
     case 'stanford_basic_pages--basic_page_type_list':
-      graphqlResponse = await client.stanfordBasicPages({filters, pageSize: itemsPerPage});
+      graphqlResponse = await client.stanfordBasicPages({filters, ...queryVariables});
       items = graphqlResponse.stanfordBasicPages?.results as unknown as NodeUnion[]
       break
 
     case 'stanford_basic_pages--viewfield_block_1':
-      graphqlResponse = await client.stanfordBasicPagesCards({filters, pageSize: itemsPerPage});
+      graphqlResponse = await client.stanfordBasicPagesCards({filters, ...queryVariables});
       items = graphqlResponse.stanfordBasicPagesCards?.results as unknown as NodeUnion[]
       break
 
     case 'stanford_events--cards':
       filters = getViewFilters(['term_node_taxonomy_name_depth', 'term_node_taxonomy_name_depth_1', 'term_node_taxonomy_name_depth_2', 'term_node_taxonomy_name_depth_3'], contextualFilter)
-      graphqlResponse = await client.stanfordEventsCardGrid({filters, pageSize: itemsPerPage});
+      graphqlResponse = await client.stanfordEventsCardGrid({filters, ...queryVariables});
       items = graphqlResponse.stanfordEventsCardGrid?.results as unknown as NodeUnion[]
       break
 
     case 'stanford_events--list_page':
       filters = getViewFilters(['term_node_taxonomy_name_depth', 'term_node_taxonomy_name_depth_1', 'term_node_taxonomy_name_depth_2', 'term_node_taxonomy_name_depth_3'], contextualFilter);
-      graphqlResponse = await client.stanfordEvents({filters, pageSize: itemsPerPage});
+      graphqlResponse = await client.stanfordEvents({filters, ...queryVariables});
       items = graphqlResponse.stanfordEvents?.results as unknown as NodeUnion[]
       break
 
     case 'stanford_events--past_events_list_block':
       filters = getViewFilters(['term_node_taxonomy_name_depth'], contextualFilter)
-      graphqlResponse = await client.stanfordEventsPastEvents({filters, pageSize: itemsPerPage});
+      graphqlResponse = await client.stanfordEventsPastEvents({filters, ...queryVariables});
       items = graphqlResponse.stanfordEventsPastEvents?.results as unknown as NodeUnion[]
       break
 
     case 'stanford_news--block_1':
       filters = getViewFilters(['term_node_taxonomy_name_depth'], contextualFilter)
-      graphqlResponse = await client.stanfordNewsDefaultList({filters, pageSize: itemsPerPage});
+      graphqlResponse = await client.stanfordNewsDefaultList({filters, ...queryVariables});
       items = graphqlResponse.stanfordNewsDefaultList?.results as unknown as NodeUnion[]
       break
 
     case 'stanford_news--vertical_cards':
-      graphqlResponse = await client.stanfordNewsCardGrid({filters, pageSize: itemsPerPage});
+      graphqlResponse = await client.stanfordNewsCardGrid({filters, ...queryVariables});
       items = graphqlResponse.stanfordNewsCardGrid?.results as unknown as NodeUnion[]
       break
 
     case 'stanford_person--grid_list_all':
       filters = getViewFilters(['term_node_taxonomy_name_depth'], contextualFilter)
-      graphqlResponse = await client.stanfordPerson({filters, pageSize: itemsPerPage});
+      graphqlResponse = await client.stanfordPerson({filters, ...queryVariables});
       items = graphqlResponse.stanfordPerson?.results as unknown as NodeUnion[]
       break
 
