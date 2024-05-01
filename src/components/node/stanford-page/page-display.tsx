2211036de46ec7a72a494820f56530081f2ff90c
Jen Breese-Kauth <jbreese@stanford.edu>
2024-05-01 23:18:09 +0000

    SUL23-431: adding an updated date on the Stanford page template

--- src/components/node/stanford-page/page-display.tsx
+++ src/components/node/stanford-page/page-display.tsx
@@ -3,12 +3,21 @@ import {NodeStanfordPage} from "@/lib/gql/__generated__/drupal.d";
 
 const StanfordPage = async ({node}: { node: NodeStanfordPage }) => {
   const fullWidth = node.layoutSelection?.id === 'stanford_basic_page_full';
+  const updated = node.changed.time;
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  const lastUpdated  = new Date(updated).toLocaleDateString(undefined, options)
 
   return (
     <article>
       {node.suPageComponents &&
         <ParagraphRows items={node.suPageComponents} fullWidth={fullWidth}/>
       }
+      <div className="rs-py-4">Last updated {lastUpdated}</div>
     </article>
   )
 }
