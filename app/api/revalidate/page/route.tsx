6202e911d1f3b1861d57469e615d4adb0aad3a25
Mike Decker <pookmish@gmail.com>
2024-05-28 11:51:52 -0700

    Added path invalidation

---
+++ app/api/revalidate/page/route.tsx
@@ -0,0 +1,16 @@
+import {NextRequest, NextResponse} from "next/server";
+import {revalidatePath} from "next/cache";
+
+export const revalidate = 0;
+
+export const GET = async (request: NextRequest) => {
+
+  const secret = request.nextUrl.searchParams.get('secret');
+  if (secret !== process.env.DRUPAL_REVALIDATE_SECRET) return NextResponse.json({message: 'Invalid token'}, {status: 403});
+
+  let path = request.nextUrl.searchParams.get('slug');
+  if (!path || path.startsWith('/node/')) return NextResponse.json({message: 'Invalid slug'}, {status: 400});
+
+  revalidatePath(path);
+  return NextResponse.json({revalidated: true, path});
+}
