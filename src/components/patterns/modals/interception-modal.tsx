47d5ee2204969f1525b28976ec935735554b7363
Jen Breese <jbreese@stanford.edu>
2024-05-29 13:06:42 -0700

    SUL23-461: Moved button for focus order (#136)
    * SUL23-461: moved button for focus order
    
    * SUL23-461: added the aria-modal=true and the hover and focus state

--- src/components/patterns/modals/interception-modal.tsx
+++ src/components/patterns/modals/interception-modal.tsx
@@ -44,19 +44,13 @@ const InterceptionModal = ({children, ...props}: PropsWithChildren<any>) => {
       <dialog
         ref={overlay}
         className="modal fixed w-screen h-full overscroll-contain overflow-y-scroll overflow-x-hidden top-0 left-0 items-center justify-center z-[10000] bg-black-true bg-opacity-[90%] flex"
+        aria-modal="true"
         onClick={onClick}
         open
         {...props}
       >
         <ReactFocusLock returnFocus>
-          <div
-            ref={wrapper}
-            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-11/12 sm:w-10/12 md:w-8/12 lg:w-1/2 p-6"
-          >
-            {children}
-          </div>
-
-          <button
+        <button
             type="button"
             onClick={onDismiss}
             className="fixed right-50 top-50 text-white flex hocus:underline"
@@ -64,6 +58,12 @@ const InterceptionModal = ({children, ...props}: PropsWithChildren<any>) => {
             Close<span className="sr-only"> Overlay</span>
             <XMarkIcon className="ml-5" width={25}/>
           </button>
+          <div
+            ref={wrapper}
+            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-11/12 sm:w-10/12 md:w-8/12 lg:w-1/2 p-6"
+          >
+            {children}
+          </div>
         </ReactFocusLock>
       </dialog>
 
