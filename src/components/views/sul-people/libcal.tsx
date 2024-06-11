7178df40a130e37da157cf27638020617dfb0fa8
Jen Breese-Kauth <jbreese@stanford.edu>
2024-06-11 23:23:56 +0000

    SUL23-441: fixes to image, layout, responsive

--- src/components/views/sul-people/libcal.tsx
+++ src/components/views/sul-people/libcal.tsx
@@ -4,7 +4,7 @@ const LibCal = ({libcalId, srText}: { libcalId?: number, srText: string }) => {
   return (
     <>
       {libcalId &&
-        <Link href={`/calendar/${libcalId}`} className="button text-black whitespace-nowrap bg-white border border-solid border-digital-red w-fit text-16 md:text-18" aria-haspopup="dialog">
+        <Link href={`/calendar/${libcalId}`} className="button text-black whitespace-nowrap bg-white border border-solid border-digital-red hocus:shadow-button hocus:bg-inherit hocus:text-black w-fit text-16 md:text-18" aria-haspopup="dialog">
           Schedule an appointment<span className="sr-only">&nbsp;for {srText}</span>
         </Link>
       }
