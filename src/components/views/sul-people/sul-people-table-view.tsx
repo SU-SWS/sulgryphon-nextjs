e76b329c8dc15f3c8bacfbb0c618a18665460e56
pookmish <pookmish@gmail.com>
2024-07-17 15:41:09 -0700

    Keyword search filter stubbed out (#156)
    * Keyword search filter stubbed out
    
    * SUL23-503: fixup to search box styles
    
    * fixup to styles
    
    * moved over the search cursor
    
    * Fix label
    
    * fixup to the button toggles
    
    ---------
    
    Co-authored-by: Jen Breese-Kauth <jbreese@stanford.edu>

--- src/components/views/sul-people/sul-people-table-view.tsx
+++ src/components/views/sul-people/sul-people-table-view.tsx
@@ -8,7 +8,8 @@ import EmailLink from "@/components/patterns/elements/email-link"
 import {Maybe, NodeStanfordPerson} from "@/lib/gql/__generated__/drupal.d"
 import {Table, Thead, Tbody, Tr, Th, Td} from "react-super-responsive-table"
 import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css"
-import {useState} from "react"
+import {useId, useRef, useState} from "react"
+import {MagnifyingGlassIcon} from "@heroicons/react/24/solid"
 
 export type TablePerson = {
   id: NodeStanfordPerson["id"]
@@ -29,8 +30,12 @@ type Props = {
 
 const SulPeopleTableView = ({items, hasHeading}: Props) => {
   const HeadingElement = hasHeading ? "h3" : "h2"
+  const id = useId()
+  const keywordRef = useRef<HTMLInputElement>(null)
 
   const [typeFilter, setTypeFilter] = useState<string[]>([])
+  const [keywordFilter, setKeywordFilter] = useState("")
+
   let displayedItems = items
 
   if (typeFilter.length >= 1) {
@@ -53,39 +58,75 @@ const SulPeopleTableView = ({items, hasHeading}: Props) => {
     })
   }
 
+  if (keywordFilter) {
+    displayedItems = displayedItems.filter(item => item.title.toLowerCase().includes(keywordFilter.toLowerCase()) || item.fullTitle?.toLowerCase().includes(keywordFilter.toLowerCase()) || item.researchAreas?.join(" ").toLowerCase().includes(keywordFilter.toLowerCase()))
+  }
+
   return (
     <div>
-      <form className="mb-32">
-        <fieldset className="mx-auto flex w-fit items-center rounded-full border border-black-80">
-          <legend className="sr-only">Filter by speciality</legend>
-          <label className="group hidden cursor-pointer border-r border-black-80 md:block">
-            <input
-              type="checkbox"
-              className="peer sr-only"
-              checked={!typeFilter.length}
-              onChange={() => updateTypeFilter()}
-            />
-            <span className="block rounded-l-full border-r-0 border-transparent p-10 px-20 text-19 underline group-hover:no-underline peer-checked:bg-[#979694] peer-checked:bg-opacity-20 peer-focus:border-2 peer-focus:border-black-80 peer-focus:bg-[#979694] peer-focus:bg-opacity-10">All specialists</span>
+      <form
+        className="mx-auto mb-32 flex w-fit flex-wrap justify-center gap-30 lg:flex-nowrap"
+        onSubmit={e => e.preventDefault()}
+      >
+        <div className="relative max-w-[350px] md:w-[435px]">
+          <label
+            className="pl-15 text-22 font-semibold"
+            htmlFor={id}
+          >
+            Search by name, title, or subject
           </label>
-          <label className="group cursor-pointer">
-            <input
-              type="checkbox"
-              className="peer sr-only"
-              checked={typeFilter.includes("subject specialist")}
-              onChange={() => updateTypeFilter("subject specialist")}
+
+          <input
+            className="type-0 block h-40 w-full rounded-full pl-15"
+            ref={keywordRef}
+            type="text"
+            id={id}
+          />
+
+          <button
+            type="submit"
+            className="absolute bottom-6 right-10 z-10"
+            onClick={() => setKeywordFilter(keywordRef.current?.value || "")}
+          >
+            <MagnifyingGlassIcon
+              className="text-digital-red-dark"
+              width={25}
             />
-            <span className="block items-center rounded-l-full border border-transparent p-10 pr-20 text-19 underline group-hover:no-underline peer-checked:bg-[#979694] peer-checked:bg-opacity-20 peer-focus:border-2 peer-focus:border-black-80 peer-focus:bg-[#979694] peer-focus:bg-opacity-10 md:rounded-l-none">Subject specialists</span>
-          </label>
-          <label className="group cursor-pointer border-l border-black-80">
-            <input
-              type="checkbox"
-              className="peer sr-only"
-              checked={typeFilter.includes("technical specialist")}
-              onChange={() => updateTypeFilter("technical specialist")}
-            />
-            <span className="block items-center rounded-r-full border border-transparent p-10 pr-20 text-19 underline group-hover:no-underline peer-checked:bg-[#979694] peer-checked:bg-opacity-20 peer-focus:border-2 peer-focus:border-black-80 peer-focus:bg-[#979694] peer-focus:bg-opacity-10">Technical specialists</span>
-          </label>
-        </fieldset>
+            <span className="sr-only">Search</span>
+          </button>
+        </div>
+        <div className="self-end">
+          <fieldset className="mx-auto flex w-fit items-center rounded-full border border-black-80">
+            <legend className="sr-only">Filter by speciality</legend>
+            <label className="group hidden cursor-pointer border-r-0 border-black-80 md:block">
+              <input
+                type="checkbox"
+                className="peer sr-only"
+                checked={!typeFilter.length}
+                onChange={() => updateTypeFilter()}
+              />
+              <span className="block rounded-l-full border-r-0 border-transparent p-10 px-20 text-19 underline group-hover:no-underline peer-checked:bg-[#979694] peer-checked:bg-opacity-20 peer-focus:border-2 peer-focus:border-black-80 peer-focus:bg-[#979694] peer-focus:bg-opacity-10">All specialists</span>
+            </label>
+            <label className="group cursor-pointer border-l border-black-80">
+              <input
+                type="checkbox"
+                className="peer sr-only"
+                checked={typeFilter.includes("subject specialist")}
+                onChange={() => updateTypeFilter("subject specialist")}
+              />
+              <span className="block items-center rounded-l-full border border-transparent p-10 pr-20 text-19 underline group-hover:no-underline peer-checked:bg-[#979694] peer-checked:bg-opacity-20 peer-focus:border-2 peer-focus:border-black-80 peer-focus:bg-[#979694] peer-focus:bg-opacity-10 md:rounded-l-none">Subject specialists</span>
+            </label>
+            <label className="group cursor-pointer border-l border-black-80">
+              <input
+                type="checkbox"
+                className="peer sr-only"
+                checked={typeFilter.includes("technical specialist")}
+                onChange={() => updateTypeFilter("technical specialist")}
+              />
+              <span className="block items-center rounded-r-full border border-transparent p-10 pr-20 text-19 underline group-hover:no-underline peer-checked:bg-[#979694] peer-checked:bg-opacity-20 peer-focus:border-2 peer-focus:border-black-80 peer-focus:bg-[#979694] peer-focus:bg-opacity-10">Technical specialists</span>
+            </label>
+          </fieldset>
+        </div>
       </form>
       <Table className="responsive-table text-center md:text-left">
         <caption
