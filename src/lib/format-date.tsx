08d9c4e18d0c599b9a549ec6f4a5087565511e6f
Mike Decker <pookmish@stanford.edu>
2024-08-06 08:58:00 -0700

    Fix date formatting when the input is UTC

--- src/lib/format-date.tsx
+++ src/lib/format-date.tsx
@@ -1,9 +1,12 @@
 export function formatDate(input: string): string {
+  // If the time is in UTC format, change it to LA timezone.
+  if (input.includes("+00:00")) input = input.replace(/\+.*$/, "-09:00")
+
   const date = new Date(input)
   return date.toLocaleDateString("en-US", {
     month: "long",
     day: "numeric",
     year: "numeric",
-    timeZone: "America/Los_Angeles"
+    timeZone: "America/Los_Angeles",
   })
 }
