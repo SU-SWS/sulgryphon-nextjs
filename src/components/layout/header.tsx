23b642794e7e45d29dccd19ac524fdae311b4953
Jen Breese-Kauth <jbreese@stanford.edu>
2024-08-13 15:59:10 +0000

    SUL23-585: button in header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,5 +1,4 @@
 import Link from "@/components/patterns/elements/drupal-link"
-import {DrupalLinkButton} from "@/components/patterns/link"
 import Lockup from "@/components/patterns/lockup"
 import MainMenu from "@/components/menu/main-menu"
 import GlobalMessage from "@/components/layout/global-message"
@@ -29,13 +28,18 @@ const Header = async () => {
           <Lockup className="pb-20" />
 
           <nav className="hidden lg:block" aria-label="User links">
-            <ul className="list-unstyled flex gap-[40px] xl:gap-[55px]">
+            <ul className="list-unstyled flex items-baseline gap-[40px] xl:gap-[55px]">
               <HeaderLink href="/library-accessibility" text="Library Accessibility" />
               <HeaderLink href="https://mylibrary.stanford.edu/" text="My Account" />
               <HeaderLink href="/contact-us" text="Contact Us" />
-              <DrupalLinkButton href="/support-stanford-libraries" className="mt-0">
-                Donate
-              </DrupalLinkButton>
+              <li>
+                <Link
+                  href="/support-stanford-libraries"
+                  className="cta-button group rs-mt-neg1 mt-0 block w-fit whitespace-nowrap rounded-full bg-cardinal-red px-16 py-6 text-18 font-normal leading-[1.5] text-white no-underline transition-colors hover:bg-cardinal-red-dark focus:bg-black-true active:bg-black-true hocus:text-white hocus:underline"
+                >
+                  Donate now
+                </Link>
+              </li>
             </ul>
           </nav>
         </div>
