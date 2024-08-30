acf1b231952675569ff1c54167463d0f72892633
Jen Breese-Kauth <jbreese@stanford.edu>
2024-08-30 19:25:58 +0000

    added the border on checked

--- src/components/views/sul-people/sul-people-table-view.tsx
+++ src/components/views/sul-people/sul-people-table-view.tsx
@@ -254,7 +254,7 @@ const ToggleOption = ({
 
       <span
         className={twMerge(
-          "peer-focus:nounderline flex items-center border border-black-80 p-4 pr-32 text-18 leading-[30px] no-underline hover:text-cardinal-red-dark hover:underline peer-checked:bg-[#979694] peer-checked:bg-opacity-20 peer-checked:pl-16 peer-checked:text-black peer-checked:transition-all peer-checked:ease-in-out peer-focus:border-2 peer-focus:border-black-80 peer-focus:bg-[#979694] peer-focus:bg-opacity-10 peer-focus:text-black peer-focus:underline sm:whitespace-nowrap peer-checked:[&_svg]:text-black",
+          "peer-focus:nounderline flex items-center border border-black-80 p-4 pr-32 text-18 leading-[30px] no-underline hover:text-cardinal-red-dark hover:underline peer-checked:border-2 peer-checked:bg-[#979694] peer-checked:bg-opacity-20 peer-checked:pl-16 peer-checked:text-black peer-checked:transition-all peer-checked:ease-in-out peer-focus:border-2 peer-focus:border-black-80 peer-focus:bg-[#979694] peer-focus:bg-opacity-10 peer-focus:text-black peer-focus:underline sm:whitespace-nowrap peer-checked:[&_svg]:text-black",
           clsx({
             "rounded-l-full": first,
             "rounded-r-full": last,
