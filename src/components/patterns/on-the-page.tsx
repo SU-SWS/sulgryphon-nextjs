e0e5ceeb2513cdf268b3ddb63d858e4da48bdb07
rebeccahongsf <rebeccahongsf@gmail.com>
2024-10-09 15:16:03 -0700

    SUL23-642 | add responsive tablet and mobile select list; tweak layout

--- src/components/patterns/on-the-page.tsx
+++ src/components/patterns/on-the-page.tsx
@@ -4,6 +4,8 @@ import React, {useState, useEffect} from "react"
 import {NodeStanfordNews, NodeStanfordPage, NodeSulLibrary} from "@/lib/gql/__generated__/drupal.d"
 import {twMerge} from "tailwind-merge"
 import DrupalLink from "./elements/drupal-link"
+import SelectList from "./elements/select-list"
+import {SelectOptionDefinition} from "@mui/base/useSelect"
 
 interface Heading {
   text: string
@@ -57,43 +59,77 @@ const OnThePageLink = ({node}: {node: NodeStanfordPage | NodeStanfordNews | Node
     }
   }, [])
 
+  const options: SelectOptionDefinition<string>[] = [
+    ...headings.map(heading => ({
+      value: heading.id,
+      label: heading.text,
+    })),
+    {
+      value: "related-links-header",
+      label: node.sulRelLinksHeading || "Related content",
+      disabled: true,
+    },
+    ...(node.sulRelLinks?.map(link => ({
+      value: String(link.url),
+      label: String(link.title),
+    })) || []),
+  ]
+
   return (
-    <div className="sticky top-0 h-fit w-300 bg-fog-light px-24 pb-40 pt-16">
-      <nav>
-        <h3 className="font-sans font-semibold">On this page</h3>
-        <ul className="list-none p-0">
-          {headings.map(heading => (
-            <li key={heading.id}>
-              <a
-                href={`#${heading.id}`}
-                className={twMerge(
-                  "type-1 block border-l-4 pl-16 font-sans font-normal leading-[36px] text-black no-underline",
-                  activeHeading === heading.id ? "border-cardinal-red" : "border-transparent"
-                )}
-              >
-                {heading.text}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      {relLinks && (
-        <div className="mt-40">
-          <h3 className="font-sans font-semibold">{relLinkHeading || "Related content"}</h3>
+    <>
+      <div className="block w-full md:w-500 lg:hidden">
+        <SelectList
+          label="On the page"
+          options={options}
+          onChange={(event, value) => {
+            if (value) {
+              const selectedHeading = document.getElementById(value as string)
+              if (selectedHeading) {
+                selectedHeading.scrollIntoView({behavior: "smooth"})
+              } else {
+                window.location.href = value as string
+              }
+            }
+          }}
+        />
+      </div>
+      <div className="sticky top-0 hidden h-fit w-300 bg-fog-light px-24 pb-40 pt-16 lg:block">
+        <nav>
+          <h3 className="font-sans font-semibold">On this page</h3>
           <ul className="list-none p-0">
-            {relLinks.map((link, index) => (
-              <li key={index}>
-                {link.url && (
-                  <DrupalLink href={link.url} className={twMerge("type-1 font-sans font-normal leading-[36px]")}>
-                    {link.title}
-                  </DrupalLink>
-                )}
+            {headings.map(heading => (
+              <li key={heading.id}>
+                <a
+                  href={`#${heading.id}`}
+                  className={twMerge(
+                    "type-1 block border-l-4 pl-16 font-sans font-normal leading-[36px] text-black no-underline",
+                    activeHeading === heading.id ? "border-cardinal-red" : "border-transparent"
+                  )}
+                >
+                  {heading.text}
+                </a>
               </li>
             ))}
           </ul>
-        </div>
-      )}
-    </div>
+        </nav>
+        {relLinks && (
+          <div className="mt-40">
+            <h3 className="font-sans font-semibold">{relLinkHeading || "Related content"}</h3>
+            <ul className="list-none p-0">
+              {relLinks.map((link, index) => (
+                <li key={index}>
+                  {link.url && (
+                    <DrupalLink href={link.url} className={twMerge("type-1 font-sans font-normal leading-[36px]")}>
+                      {link.title}
+                    </DrupalLink>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </>
   )
 }
 
