5af52e8fbdb56b89890dab295778df3ee97f28c8
rebeccahongsf <rebeccahongsf@gmail.com>
2024-10-10 15:58:25 -0700

    remove unused peer-focus:nounderline typo

--- src/components/patterns/toggle-option.tsx
+++ src/components/patterns/toggle-option.tsx
@@ -25,7 +25,7 @@ const ToggleOption = ({
 
       <span
         className={twMerge(
-          "peer-focus:nounderline flex items-center border border-black-80 p-4 pl-0 pr-32 text-18 leading-[25px] no-underline hover:text-cardinal-red-dark hover:underline peer-checked:border-2 peer-checked:bg-[#979694] peer-checked:bg-opacity-20 peer-checked:pl-10 peer-checked:pr-13 peer-checked:text-black peer-checked:no-underline peer-checked:transition-all peer-checked:ease-in-out peer-focus:border-2 peer-focus:border-black-80 peer-focus:bg-[#979694] peer-focus:bg-opacity-10 peer-focus:text-black peer-focus:underline peer-focus-visible:outline peer-focus-visible:outline-blue-500 peer-focus-visible:ring sm:whitespace-nowrap sm:leading-[30px] sm:peer-checked:pl-16 sm:peer-checked:pr-32 peer-checked:[&_svg]:text-black",
+          "flex items-center border border-black-80 p-4 pl-0 pr-32 text-18 leading-[25px] no-underline hover:text-cardinal-red-dark hover:underline peer-checked:border-2 peer-checked:bg-[#979694] peer-checked:bg-opacity-20 peer-checked:pl-10 peer-checked:pr-13 peer-checked:text-black peer-checked:no-underline peer-checked:transition-all peer-checked:ease-in-out peer-focus:border-2 peer-focus:border-black-80 peer-focus:bg-[#979694] peer-focus:bg-opacity-10 peer-focus:text-black peer-focus:underline peer-focus-visible:outline peer-focus-visible:outline-blue-500 peer-focus-visible:ring sm:whitespace-nowrap sm:leading-[30px] sm:peer-checked:pl-16 sm:peer-checked:pr-32 peer-checked:[&_svg]:text-black",
           clsx({
             "rounded-l-full": first,
             "rounded-r-full": last,
