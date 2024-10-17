377415c4fc594ebdd8713e663741c25842fc5e2d
rebeccahongsf <rebeccahongsf@gmail.com>
2024-10-17 12:56:26 -0700

    SUL23-650 | update font to match mocks

--- src/components/views/sul-study-place/filtering-table/study-place-filtering-table.tsx
+++ src/components/views/sul-study-place/filtering-table/study-place-filtering-table.tsx
@@ -253,7 +253,7 @@ const StudyPlaceFilteringTable = ({items}: Props) => {
                       </div>
                     </a>
                   )}
-                  {!item.libCalId && <p className="m-0">Reservation not required</p>}
+                  {!item.libCalId && <p className="m-0 text-16">Reservation not required</p>}
                 </Td>
               </Tr>
             ))}
