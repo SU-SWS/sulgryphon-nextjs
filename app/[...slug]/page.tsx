faafba9e49240d2ab9c3540669184709085fdd90
rebeccahongsf <rebeccahongsf@gmail.com>
2024-10-23 13:47:43 -0700

    SUL23-654 | swap out article for div and add article wrapper to entire main content

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -36,82 +36,84 @@ const NodePage = async ({params, previewMode}: PageProps) => {
 
   return (
     <main id="main-content" className="mb-50">
-      {process.env.VERCEL_ENV !== "production" && <FlushCache currentPath={path} />}
+      <article>
+        {process.env.VERCEL_ENV !== "production" && <FlushCache currentPath={path} />}
 
-      {!entity.status && <EditorAlertBanner message="Unpublished Content" />}
-      {entity.__typename === "NodeSulLibrary" && <LibraryHeader node={entity} />}
+        {!entity.status && <EditorAlertBanner message="Unpublished Content" />}
+        {entity.__typename === "NodeSulLibrary" && <LibraryHeader node={entity} />}
 
-      {entity.__typename === "NodeStanfordNews" && (
-        <InternalHeaderBanner>
-          <div className="mx-auto mb-50 mt-80 flex w-full max-w-[calc(100vw-10rem)] flex-col p-0 md:mt-100 md:max-w-[calc(100vw-20rem)] 3xl:max-w-[calc(1500px-20rem)]">
-            <h1 id={entity.id} className="order-2 text-white">
-              {entity.title}
-            </h1>
+        {entity.__typename === "NodeStanfordNews" && (
+          <InternalHeaderBanner>
+            <div className="mx-auto mb-50 mt-80 flex w-full max-w-[calc(100vw-10rem)] flex-col p-0 md:mt-100 md:max-w-[calc(100vw-20rem)] 3xl:max-w-[calc(1500px-20rem)]">
+              <h1 id={entity.id} className="order-2 text-white">
+                {entity.title}
+              </h1>
 
-            {entity.suNewsTopics && (
-              <div className="order-1 mb-20">
-                {entity.suNewsTopics.slice(0, 1).map(topic => (
-                  <span key={topic.id} className="font-semibold text-illuminating-dark">
-                    {topic.name}
-                  </span>
-                ))}
-              </div>
-            )}
-          </div>
-        </InternalHeaderBanner>
-      )}
-
-      {!(entity.__typename === "NodeSulLibrary" || entity.__typename === "NodeStanfordNews") && (
-        <InternalHeaderBanner>
-          <h1
-            id={entity.id}
-            className="relative mx-auto mb-50 mt-80 w-full max-w-[calc(100vw-10rem)] p-0 text-white md:mt-100 md:max-w-[calc(100vw-20rem)] 3xl:max-w-[calc(1500px-20rem)]"
-          >
-            {entity.title}
-          </h1>
-        </InternalHeaderBanner>
-      )}
-
-      {fullWidth && (
-        <div>
-          <NodePageDisplay node={entity} />
-        </div>
-      )}
-
-      {!fullWidth && (
-        <div className="centered flex flex-col justify-between gap-[8rem] lg:flex-row">
-          <div className="order-last flex-1">
-            <NodePageDisplay node={entity} aria-labelledby={entity.id} />
-          </div>
-
-          {sulSidebar && (
-            <OnThisPage>
-              {entity.sulRelLinks && (
-                <div className="lg:mt-40">
-                  <h3 className="type-0 m-0 block px-10 py-2 font-sans font-semibold leading-[30px] text-cardinal-red lg:type-1 lg:mb-8 lg:p-0 lg:text-black">
-                    {entity.sulRelLinksHeading || "Related content"}
-                  </h3>
-                  <ul className="list-none p-0">
-                    {entity.sulRelLinks.map((link, index) => (
-                      <li key={index} className="mb-0 lg:mb-12">
-                        {link.url && (
-                          <DrupalLink
-                            href={link.url}
-                            className="type-0 block break-words px-10 py-2 font-sans font-normal leading-[30px] text-black no-underline hocus:bg-black-10 hocus:underline lg:p-0 lg:text-digital-blue lg:underline lg:hocus:bg-transparent"
-                          >
-                            {link.title}
-                          </DrupalLink>
-                        )}
-                      </li>
-                    ))}
-                  </ul>
+              {entity.suNewsTopics && (
+                <div className="order-1 mb-20">
+                  {entity.suNewsTopics.slice(0, 1).map(topic => (
+                    <span key={topic.id} className="font-semibold text-illuminating-dark">
+                      {topic.name}
+                    </span>
+                  ))}
                 </div>
               )}
-            </OnThisPage>
-          )}
-          {!sulSidebar && <SecondaryMenu menuItems={menuItems} currentPath={entity.path} />}
-        </div>
-      )}
+            </div>
+          </InternalHeaderBanner>
+        )}
+
+        {!(entity.__typename === "NodeSulLibrary" || entity.__typename === "NodeStanfordNews") && (
+          <InternalHeaderBanner>
+            <h1
+              id={entity.id}
+              className="relative mx-auto mb-50 mt-80 w-full max-w-[calc(100vw-10rem)] p-0 text-white md:mt-100 md:max-w-[calc(100vw-20rem)] 3xl:max-w-[calc(1500px-20rem)]"
+            >
+              {entity.title}
+            </h1>
+          </InternalHeaderBanner>
+        )}
+
+        {fullWidth && (
+          <div>
+            <NodePageDisplay node={entity} />
+          </div>
+        )}
+
+        {!fullWidth && (
+          <div className="centered flex flex-col justify-between gap-[8rem] lg:flex-row">
+            <div className="order-last flex-1">
+              <NodePageDisplay node={entity} aria-labelledby={entity.id} />
+            </div>
+
+            {sulSidebar && (
+              <OnThisPage>
+                {entity.sulRelLinks && (
+                  <div className="lg:mt-40">
+                    <h3 className="type-0 m-0 block px-10 py-2 font-sans font-semibold leading-[30px] text-cardinal-red lg:type-1 lg:mb-8 lg:p-0 lg:text-black">
+                      {entity.sulRelLinksHeading || "Related content"}
+                    </h3>
+                    <ul className="list-none p-0">
+                      {entity.sulRelLinks.map((link, index) => (
+                        <li key={index} className="mb-0 lg:mb-12">
+                          {link.url && (
+                            <DrupalLink
+                              href={link.url}
+                              className="type-0 block break-words px-10 py-2 font-sans font-normal leading-[30px] text-black no-underline hocus:bg-black-10 hocus:underline lg:p-0 lg:text-digital-blue lg:underline lg:hocus:bg-transparent"
+                            >
+                              {link.title}
+                            </DrupalLink>
+                          )}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </OnThisPage>
+            )}
+            {!sulSidebar && <SecondaryMenu menuItems={menuItems} currentPath={entity.path} />}
+          </div>
+        )}
+      </article>
     </main>
   )
 }
