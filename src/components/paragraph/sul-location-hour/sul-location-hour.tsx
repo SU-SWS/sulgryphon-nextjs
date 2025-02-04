9eca960b9ab03c99b9e83b5999c7e5318f02a6b2
Mike Decker <pookmish@stanford.edu>
2025-02-04 13:00:26 -0800

    Support links in location hour paragraph component

--- src/components/paragraph/sul-location-hour/sul-location-hour.tsx
+++ src/components/paragraph/sul-location-hour/sul-location-hour.tsx
@@ -2,6 +2,7 @@ import {HTMLAttributes} from "react"
 import {NodeSulLibrary, ParagraphSulLocationHour} from "@/lib/gql/__generated__/drupal.d"
 import {graphqlClient} from "@/lib/gql/fetcher"
 import SulLocationHoursClient from "@/components/paragraph/sul-location-hour/sul-location-hours.client"
+import formatHtml from "@/lib/format-html"
 
 type Props = HTMLAttributes<HTMLDivElement> & {
   paragraph: ParagraphSulLocationHour
@@ -10,6 +11,12 @@ const SulLocationHour = async ({paragraph, ...props}: Props) => {
   const librariesQuery = await graphqlClient({next: {tags: ["node:sul_library"]}}).Libraries()
   const libraries = librariesQuery.nodeSulLibraries.nodes.filter(node => !!node.suLibraryHours) as NodeSulLibrary[]
 
-  return <SulLocationHoursClient libraries={libraries} alert={paragraph.sulLocHoursAlert?.processed} {...props} />
+  return (
+    <SulLocationHoursClient
+      libraries={libraries}
+      alert={formatHtml(paragraph.sulLocHoursAlert?.processed)}
+      {...props}
+    />
+  )
 }
 export default SulLocationHour
--- src/components/paragraph/sul-location-hour/sul-location-hours.client.tsx
+++ src/components/paragraph/sul-location-hour/sul-location-hours.client.tsx
@@ -4,7 +4,7 @@ import Card from "@/components/patterns/card"
 import {ClockIcon} from "@heroicons/react/24/outline"
 import {MapPinIcon} from "@heroicons/react/24/solid"
 import Image from "next/image"
-import {HTMLAttributes, useId, useState} from "react"
+import {HTMLAttributes, ReactNode, useId, useState} from "react"
 import {ErrorBoundary} from "react-error-boundary"
 import CachedClientFetch from "@/components/utils/cached-client-fetch"
 import useTodayLibraryHours from "@/lib/hooks/useTodayLibraryHours"
@@ -16,7 +16,7 @@ import MoonStarsIcon from "@/components/patterns/icons/MoonStarsIcon"
 
 type HoursProps = HTMLAttributes<HTMLDivElement> & {
   libraries: NodeSulLibrary[]
-  alert?: string
+  alert?: string | ReactNode
 }
 
 const SulLocationHoursClient = ({libraries, alert, ...props}: HoursProps) => {
@@ -35,7 +35,14 @@ interface option {
   label: string
 }
 
-const LibrariesTodayHours = ({libraries, alert, ...props}: {libraries: HoursProps["libraries"]; alert?: string}) => {
+const LibrariesTodayHours = ({
+  libraries,
+  alert,
+  ...props
+}: {
+  libraries: HoursProps["libraries"]
+  alert?: string | ReactNode
+}) => {
   const formId = useId()
   const [selectedLibrary, setSelectedLibrary] = useState(
     libraries.find(library => library.suLibraryHours === "green")?.id ?? libraries[0].id
@@ -66,7 +73,7 @@ const LibrariesTodayHours = ({libraries, alert, ...props}: {libraries: HoursProp
             <span className="absolute bottom-0 z-10 w-full bg-cardinal-red p-10">
               <span className="mx-auto flex w-fit items-center gap-10 text-12 font-semibold leading-normal text-white sm:text-16">
                 <MoonStarsIcon className="ml-10" />
-                {alert}
+                <span className="[&_a:hocus]:text-black [&_a]:text-white">{alert}</span>
               </span>
             </span>
           </span>
