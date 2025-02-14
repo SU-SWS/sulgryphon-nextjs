2d8dcf2c3d05b000c6a7779a8e4257635f2c08b7
rebeccahongsf <rebeccahongsf@gmail.com>
2025-02-14 12:39:21 -0800

    add object-contain

--- src/components/layout/library-footer.tsx
+++ src/components/layout/library-footer.tsx
@@ -24,7 +24,13 @@ const LibraryFooter = () => {
         </div>
         <div className="absolute bottom-50 right-[-5rem] block @3xl:right-[-3rem] @5xl:bottom-0 @5xl:right-[2rem] @8xl:right-[8rem]">
           <div className="relative h-[18.5rem] w-180 @5xl:h-[20.5rem] @5xl:w-200 @8xl:h-[28.4rem] @8xl:w-[27.7rem]">
-            <Image src="/footer-nerd-squirrel.png" alt="" fill sizes="(max-width: 300px) 100vw, 300px" />
+            <Image
+              src="/footer-nerd-squirrel.png"
+              alt=""
+              className="object-contain"
+              fill
+              sizes="(max-width: 300px) 100vw, 300px"
+            />
           </div>
         </div>
       </div>
