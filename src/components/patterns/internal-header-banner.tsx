92e2f5ec47d81f096e6db97609ccf9a011f2e319
Rebecca Hong <34019925+rebeccahongsf@users.noreply.github.com>
2025-04-14 10:58:49 -0700

    SUL23-717 SUL23-718 SUL23-719 SUL23-387  | implement site improve and wave fixes for accessibility (#287)
    * SUL23-719 | increase contact details and information text size
    
    * SUL23-717 | remove duplicated aria-labelby
    
    * SUL23-717 | add aria-labelby for specific content types
    
    * SUL23-718 | replace overflow hidden with auto
    
    * SUL23-387 | adjust to conditionally display title attribute only if it doesn't exist in display text to reduce duplication
    
    * revert nodes index and update div to article for StanfordPage
    
    * revert app slug page

--- src/components/patterns/internal-header-banner.tsx
+++ src/components/patterns/internal-header-banner.tsx
@@ -3,7 +3,7 @@ import {PropsWithChildren} from "react"
 
 const InternalHeaderBanner = ({children}: PropsWithChildren) => {
   return (
-    <header className="relative mb-50 overflow-hidden bg-black-true">
+    <header className="relative mb-50 overflow-auto bg-black-true">
       <div className="relative z-[1]">{children}</div>
 
       <div className="absolute bottom-0 right-0 h-2/3 w-1/2 bg-right-bottom lg:bg-interior-header-sprinkles">
