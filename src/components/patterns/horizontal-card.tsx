5ce1f656a00088c53efbd19cd10174d3babb4c9f
rebeccahongsf <rebeccahongsf@gmail.com>
2025-05-01 08:51:32 -0700

    fixup conditionals and remove unused div

--- src/components/patterns/horizontal-card.tsx
+++ src/components/patterns/horizontal-card.tsx
@@ -6,6 +6,7 @@ import {ElementType, HTMLAttributes, useRef} from "react"
 import Link from "@/components/patterns/elements/drupal-link"
 import {Maybe, Link as LinkType} from "@/lib/gql/__generated__/drupal.d"
 import {twMerge} from "tailwind-merge"
+import {clsx} from "clsx"
 
 type CardProps = HTMLAttributes<HTMLDivElement> & {
   video?: Maybe<ReactNodeLike>
@@ -47,20 +48,20 @@ const HorizontalCard = ({
     linkAttributes["aria-labelledby"] = headerId
     delete linkAttributes["aria-label"]
   }
-  let bgColor = "bg-fog-light text-black-true"
-  let btnColor = "bg-cardinal-red text-white border-cardinal-red hocus:bg-black-true hocus:text-white"
-  let linkColor = ""
-
-  switch (cardBgColor) {
-    case "cardinal_red":
-      bgColor = "bg-cardinal-red text-white"
-      btnColor = "bg-white text-cardinal-red hocus:bg-black-true hocus:text-white border-white"
-      linkColor = "[&_a]:text-white hocus:[&_a]:text-black-true"
-      break
-  }
 
   return (
-    <div {...props} ref={ref} className={twMerge("relative", bgColor, props.className)}>
+    <div
+      {...props}
+      ref={ref}
+      className={twMerge(
+        "relative",
+        clsx({
+          "bg-cardinal-red text-white": cardBgColor === "cardinal_red",
+          "bg-fog-light text-black-true": cardBgColor !== "cardinal_red",
+        }),
+        props.className
+      )}
+    >
       <div className="rs-p-5 relative w-full leading-display @container @6xl:centered lg:px-80">
         <div className="grid items-center gap-2xl @6xl:grid-cols-2 @10xl:gap-[8.8rem]">
           {(image || video) && (
@@ -76,43 +77,54 @@ const HorizontalCard = ({
               )}
             </div>
           )}
+          <div className="mb-16 flex flex-row flex-wrap items-center gap-16 @6xl:flex-nowrap">
+            <Image src="/card-rosette.png" alt="" className="object-contain" height={80} width={80} />
+            <div>
+              {superHeader && (
+                <span className="mb-0 text-20 font-semibold uppercase leading-display">{superHeader}</span>
+              )}
 
-          <div>
-            <div className="mb-16 flex flex-row flex-wrap items-center gap-16 @6xl:flex-nowrap">
-              <Image src="/card-rosette.png" alt="" className="object-contain" height={80} width={80} />
-              <div>
-                {superHeader && (
-                  <span className="mb-0 text-20 font-semibold uppercase leading-display">{superHeader}</span>
-                )}
-
-                {header && (
-                  <Heading
-                    id={headerId}
-                    className={twMerge("word-break lg:text-32 mb-0 text-26 md:text-28", hideHeading && "sr-only")}
-                  >
-                    {header}
-                  </Heading>
-                )}
-              </div>
-            </div>
-            <div className="m-0 @10xl:rs-ml-2">
-              {body && <div className={twMerge("[&_p]:text-20", linkColor)}>{formatHtml(body)}</div>}
-
-              {footer && <div className="rs-pt-0 text-18 font-normal leading-display text-digital-red">{footer}</div>}
-
-              {link?.url && (
-                <Link
-                  href={link.url}
-                  className={twMerge(
-                    "cta-button group mt-32 block w-fit rounded-full border-2 px-26 pb-11 pt-10 text-24 font-semibold leading-display no-underline transition-colors hocus:underline md:text-18",
-                    btnColor
-                  )}
-                  {...linkAttributes}
+              {header && (
+                <Heading
+                  id={headerId}
+                  className={twMerge("word-break lg:text-32 mb-0 text-26 md:text-28", hideHeading && "sr-only")}
                 >
-                  {link.title}
-                </Link>
+                  {header}
+                </Heading>
               )}
             </div>
+          </div>
+          <div className="m-0 @10xl:rs-ml-2">
+            {body && (
+              <div
+                className={twMerge(
+                  "[&_p]:text-20",
+                  cardBgColor === "cardinal_red" && "[&_a]:text-white hocus:[&_a]:text-black-true"
+                )}
+              >
+                {formatHtml(body)}
+              </div>
+            )}
+
+            {footer && <div className="rs-pt-0 text-18 font-normal leading-display text-digital-red">{footer}</div>}
+
+            {link?.url && (
+              <Link
+                href={link.url}
+                className={twMerge(
+                  "cta-button group mt-32 block w-fit rounded-full border-2 px-26 pb-11 pt-10 text-24 font-semibold leading-display no-underline transition-colors hocus:underline md:text-18",
+                  clsx({
+                    "border-white bg-white text-cardinal-red hocus:bg-black-true hocus:text-white":
+                      cardBgColor === "cardinal_red",
+                    "border-cardinal-red bg-cardinal-red text-white hocus:bg-black-true hocus:text-white":
+                      cardBgColor !== "cardinal_red",
+                  })
+                )}
+                {...linkAttributes}
+              >
+                {link.title}
+              </Link>
+            )}
           </div>
         </div>
       </div>
