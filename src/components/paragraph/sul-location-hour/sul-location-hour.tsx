0fce6c0e00e950e82c6d946dc42d232c2dd47656
Rebecca Hong <34019925+rebeccahongsf@users.noreply.github.com>
2025-08-22 14:49:32 -0700

    SUL23-841 | update library hours alert to conditionally display (#324)

--- src/components/paragraph/sul-location-hour/sul-location-hour.tsx
+++ src/components/paragraph/sul-location-hour/sul-location-hour.tsx
@@ -2,7 +2,6 @@ import {HTMLAttributes} from "react"
 import {NodeSulLibrary, ParagraphSulLocationHour} from "@/lib/gql/__generated__/drupal.d"
 import {graphqlClient} from "@/lib/gql/fetcher"
 import SulLocationHoursClient from "@/components/paragraph/sul-location-hour/sul-location-hours.client"
-import formatHtml from "@/lib/format-html"
 
 type Props = HTMLAttributes<HTMLDivElement> & {
   paragraph: ParagraphSulLocationHour
@@ -11,12 +10,6 @@ const SulLocationHour = async ({paragraph, ...props}: Props) => {
   const librariesQuery = await graphqlClient({next: {tags: ["node:sul_library"]}}).Libraries()
   const libraries = librariesQuery.nodeSulLibraries.nodes.filter(node => !!node.suLibraryHours) as NodeSulLibrary[]
 
-  return (
-    <SulLocationHoursClient
-      libraries={libraries}
-      alert={formatHtml(paragraph.sulLocHoursAlert?.processed)}
-      {...props}
-    />
-  )
+  return <SulLocationHoursClient libraries={libraries} alert={paragraph.sulLocHoursAlert} {...props} />
 }
 export default SulLocationHour
