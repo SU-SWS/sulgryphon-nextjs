3ae6f9dbd0abd5e09eecad7d52b721742ff93e4d
Rebecca Hong <34019925+rebeccahongsf@users.noreply.github.com>
2025-10-22 07:50:01 -0700

    SUL23-876 | add gradient line to list headings and move buttons to beneath content (#350)
    * SUL23-876 | add gradient line to list headings and move buttons to beneath content
    
    * adjust imports and props spread location
    
    * cleanup className type and adjust the link props
    
    * omit title from html anchor element

--- src/components/paragraph/sul-featured-collection.tsx
+++ src/components/paragraph/sul-featured-collection.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import AboveHeaderBorder from "@/components/patterns/above-header-border"
 import Wave from "@/components/patterns/wave"
 import {DrupalLink} from "@/components/patterns/link"
 import {HTMLAttributes, useRef} from "react"
@@ -10,6 +9,8 @@ import Oembed from "@/components/patterns/elements/oembed"
 import Image from "next/image"
 import {buildUrl} from "@/lib/drupal/utils"
 import {MediaImage, ParagraphStanfordCard, Maybe, Link as LinkType} from "@/lib/gql/__generated__/drupal.d"
+import HeaderGradientLine from "@/components/patterns/header-gradient-line"
+import {twMerge} from "tailwind-merge"
 
 type Props = HTMLAttributes<HTMLTableSectionElement> & {
   headline?: Maybe<string>
@@ -35,28 +36,13 @@ const SulFeaturedCollection = ({headerId, headline, link, cards, styles, fullWid
 
   return (
     <section className="centered relative" ref={ref} {...props}>
-      {(headline || link?.url) && (
-        <>
-          <AboveHeaderBorder />
-          <header className="mb-40 gap-2xl md:flex">
-            {headline && (
-              <h2 id={headerId} className="mb-0 flex-grow">
-                {headline}
-              </h2>
-            )}
-
-            {link?.url && (
-              <div>
-                <DrupalLink
-                  url={link?.url}
-                  title={link?.title}
-                  linkStyle={styles?.link_display_style}
-                  {...linkAttributes}
-                />
-              </div>
-            )}
-          </header>
-        </>
+      {headline && (
+        <header className="mb-40 flex flex-row items-center justify-between gap-16">
+          <h2 id={headerId} className="mb-0 shrink-0">
+            {headline}
+          </h2>
+          <HeaderGradientLine />
+        </header>
       )}
 
       <div className="relative @container">
@@ -97,6 +83,16 @@ const SulFeaturedCollection = ({headerId, headline, link, cards, styles, fullWid
           </OnlyIfCentered>
         )}
       </div>
+
+      {link?.url && (
+        <DrupalLink
+          {...linkAttributes}
+          url={link?.url}
+          title={link?.title}
+          linkStyle={styles?.link_display_style}
+          className={twMerge("mx-auto mt-0 mt-40", linkAttributes?.className)}
+        />
+      )}
     </section>
   )
 }
