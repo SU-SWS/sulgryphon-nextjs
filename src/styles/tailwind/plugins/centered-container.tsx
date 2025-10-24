32e37267f48cabab403fcffb258ed15d93540475
rebeccahongsf <rebeccahongsf@gmail.com>
2025-10-23 19:44:36 -0700

    revert centered container

--- src/styles/tailwind/plugins/centered-container.tsx
+++ src/styles/tailwind/plugins/centered-container.tsx
@@ -1,19 +1,14 @@
-// @ts-nocheck
-
-// eslint-disable-next-line valid-jsdoc
 /**
  * Centered container.
  */
 module.exports = function () {
   return function ({addComponents}) {
     const maxWidths = {}
+
     const gutterSizes = [
       {screen: "lg", gutterSize: 80},
       {screen: "xl", gutterSize: 100},
     ]
-
-    // Find the smallest gutter for the .gutters class
-    const smallestGutter = Math.min(...gutterSizes.map(sg => sg.gutterSize), 50) // 50 is the default (100px / 2)
 
     gutterSizes.map(screenGutter => {
       maxWidths["@screen " + screenGutter.screen] = {
@@ -32,13 +27,6 @@ module.exports = function () {
         "@media only screen and (min-width: 1700px)": {
           maxWidth: "1500px",
         },
-      },
-      ".gutters": {
-        width: "100%",
-        marginLeft: "auto",
-        marginRight: "auto",
-        maxWidth: "calc(100vw - " + smallestGutter * 2 + "px)",
-        ...maxWidths,
       },
     }
 
